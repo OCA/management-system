6c75d11d57cb242604f9b35fb49aad840185579c
Maxime Chambreuil <maxime.chambreuil@savoirfairelinux.com>
2012-03-09 18:47:59 -0500

    [ADD] Initial commit. Import from extra-addons.
    [#9439] Declared the new model mgmtsystem.system.
     * Declared the menu and action.
     * Added wiki.wiki as a dependency.
    
    TODO: Create a constraint in the manual field to only allow manual pages.
    
    * multi company
    
    [FIX] default company_id
    
    [FIX] group_mgmtsystem_manager XML ID
    [IMP] mgmtsystem_kpi
    
    [FIX] Group references. Reported by michaelgiesbrecht on the forum.
    
    [ADD] Icons
    
    [IMP] Move audittrail dependency from mgmtsystem to requiring modules
    
    [IMP] security settings, system views

---
+++ mgmtsystem/mgmtsystem_system.py
@@ -0,0 +1,39 @@
+# -*- encoding: utf-8 -*-
+##############################################################################
+#    
+#    OpenERP, Open Source Management Solution
+#    Copyright (C) 2012 Savoir-faire Linux (<http://www.savoirfairelinux.com>).
+#
+#    This program is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU General Public License as
+#    published by the Free Software Foundation, either version 3 of the
+#    License, or (at your option) any later version.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU General Public License for more details.
+#
+#    You should have received a copy of the GNU General Public License
+#    along with this program.  If not, see <http://www.gnu.org/licenses/>.     
+#
+##############################################################################
+
+from osv import fields, osv
+
+class mgmtsystem_system(osv.osv):
+
+    _name = 'mgmtsystem.system'
+    description = 'System'
+
+    _columns = {
+        'name': fields.char('System', size=30, required=True),
+        'manual': fields.many2one('wiki.wiki', 'Manual'),
+        'company_id': fields.many2one('res.company', 'Company')
+        }
+
+    _defaults = {
+        'company_id': lambda self, cr, uid, c: self.pool.get('res.users').browse(cr, uid, uid, c).company_id.id,
+    }
+
+mgmtsystem_system()
