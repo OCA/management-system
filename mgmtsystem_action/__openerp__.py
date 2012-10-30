888e7e6bd0a4d7d356dd7840804746acae7e4d2b
Maxime Chambreuil <maxime.chambreuil@savoirfairelinux.com>
2012-10-30 16:42:30 -0400

    [FIX[ licenses and dependencies

--- mgmtsystem_action/__openerp__.py
+++ mgmtsystem_action/__openerp__.py
@@ -23,7 +23,7 @@
     "version" : "0.2",
     "author" : "Savoir-faire Linux",
     "website" : "http://www.savoirfairelinux.com",
-    "license" : "GPL-3",
+    "license" : "AGPL-3",
     "category" : "Management System",
     "description": """
 	This module enables you to manage the different actions of your management 
