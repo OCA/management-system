ffb9aeec4d9284e9849c39159e7d055674397d1c
Maxime Chambreuil <maxime.chambreuil@savoirfairelinux.com>
2012-10-30 16:41:04 -0400

    [REN] wiki_* to document_page_*

---
+++ document_page_environment_manual/__openerp__.py
@@ -0,0 +1,38 @@
+# -*- encoding: utf-8 -*-
+##############################################################################
+#    
+#    OpenERP, Open Source Management Solution
+#    Copyright (C) 2010 Savoir-faire Linux (<http://www.savoirfairelinux.com>).
+#
+#    This program is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU General Public License as
+#    published by the Free Software Foundation, either version 3 of the
+#    License, or (at your option) any later version.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU General Public License for more details.
+#
+#    You should have received a copy of the GNU General Public License
+#    along with this program.  If not, see <http://www.gnu.org/licenses/>.     
+#
+##############################################################################
+{
+    "name" : "Environment Manual",
+    "version" : "0.1",
+    "author" : "Savoir-faire Linux",
+    "website" : "http://www.savoirfairelinux.com",
+    "license" : "AGPL-3",
+    "category" : "Generic Modules/Others",
+    "description": """Environment Manual Template
+    """,
+    "depends" : ['document_page','mgmtsystem_manuals'],
+    "init_xml" : [],
+    "update_xml" : ['document_page_environment_manual.xml'],
+    "demo_xml" : [],
+    "installable" : True,
+    "certificate" : ''
+}
+# vim:expandtab:smartindent:tabstop=4:softtabstop=4:shiftwidth=4:
+
