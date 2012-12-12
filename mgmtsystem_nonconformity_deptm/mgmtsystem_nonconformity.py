a82f7519d652394ec3e0490ea7cdd73d79330d72
Daniel Reis <dreis.pt@hotmail.com>
2012-12-12 14:46:22 +0000

    ADD Manager's superior

--- mgmtsystem_nonconformity_deptm/mgmtsystem_nonconformity.py
+++ mgmtsystem_nonconformity_deptm/mgmtsystem_nonconformity.py
@@ -24,6 +24,7 @@ class mgmtsystem_nonconformity(osv.osv):
     _inherit = "mgmtsystem.nonconformity"
     _columns = {
         'department_id': fields.many2one('hr.department', 'Department', required=True),
+        'superior_user_id': fields.many2one('res.users','Superior', required=True),
     }
 
     def onchange_department_id(self, cr, uid, ids, new_id, context=None):
@@ -31,9 +32,9 @@ class mgmtsystem_nonconformity(osv.osv):
         if new_id:
             deptm = self.pool.get('hr.department').browse(cr, uid, new_id, context=context)
             if deptm.manager_id and deptm.manager_id.user_id:
-                result['responsible_user_id'] = deptm.manager_id.user_id.id
+                result['manager_user_id'] = deptm.manager_id.user_id.id
             if deptm.parent_id and deptm.parent_id.manager_id and deptm.parent_id.manager_id.user_id:
-                result['manager_user_id'] = deptm.parent_id.manager_id.user_id.id
+                result['superior_user_id'] = deptm.parent_id.manager_id.user_id.id
         return {'value': result}
 
 mgmtsystem_nonconformity()
