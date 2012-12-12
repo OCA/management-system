11b24517614b09fbdf398daaa057207b2f0b7510
Daniel Reis <dreis.pt@hotmail.com>
2012-12-12 10:42:04 +0000

    [FIX+IMP] mgmtsystem_nonconformity: License, translation and other stuff
    * CHG Renamed 'feedback' to 'claim', see explanation  in __openerp__.py
    * CHG pt_pt translation
    * FIX Fixing fields read-only flags based on state
    * CHG Improving action <-> NC relationship (wip)
    * [FIX] fixes from review comments
    * [FIX] missing context; instanciation of models
    * [CHG] AGPL license; set verion to 1.0
    * [CHG] Selections use words instead of letters; fixed AGPL-3 reference
    * [FIX] .py files licenses to AGPL

--- mgmtsystem_nonconformity/__openerp__.py
+++ mgmtsystem_nonconformity/__openerp__.py
@@ -5,29 +5,39 @@
 #    Copyright (C) 2010 Savoir-faire Linux (<http://www.savoirfairelinux.com>).
 #
 #    This program is free software: you can redistribute it and/or modify
-#    it under the terms of the GNU General Public License as
+#    it under the terms of the GNU Affero General Public License as
 #    published by the Free Software Foundation, either version 3 of the
 #    License, or (at your option) any later version.
 #
 #    This program is distributed in the hope that it will be useful,
 #    but WITHOUT ANY WARRANTY; without even the implied warranty of
 #    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-#    GNU General Public License for more details.
+#    GNU Affero General Public License for more details.
 #
-#    You should have received a copy of the GNU General Public License
-#    along with this program.  If not, see <http://www.gnu.org/licenses/>.     
+#    You should have received a copy of the GNU Affero General Public License
+#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #
 ##############################################################################
 {
     "name" : "Management System - Nonconformity",
-    "version" : "0.2",
+    "version" : "1.0",
     "author" : "Savoir-faire Linux",
     "website" : "http://www.savoirfairelinux.com",
+<<<<<<< ff56ec7c15f83ab383a3b19f8db21365dddbb53f
+<<<<<<< 7ae63b3eb748c2c03907bd9e7a6ab8fb298c81df
     "license" : "AGPL-3",
+=======
+    "license" : "AGPL",
+>>>>>>> [CHG] AGPL license; set verion to 1.0
+=======
+    "license" : "AGPL-3",
+>>>>>>> [CHG] Selections use words instead of letters; fixed AGPL-3 reference
     "category" : "Management System",
     "description": """\
 This module enables you to manage the nonconformities of your management 
 system : quality (ISO9001), environment (ISO14001) or security (ISO27001).	
+
+WARNING: when upgrading from v0.1, data conversion is required, since there are subtancial changes to the data structure.
     """,
     "depends" : [
         'mgmtsystem_action',
