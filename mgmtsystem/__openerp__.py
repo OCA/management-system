ccb843c097496c43b7b568dc5611e1a032b84fe3
Daniel Reis <dreis.pt@hotmail.com>
2012-12-17 16:19:36 +0000

    [CHG] AGPL license; set verion to 1.0

--- mgmtsystem/__openerp__.py
+++ mgmtsystem/__openerp__.py
@@ -5,25 +5,29 @@
 #    Copyright (C) 2010 Savoir-faire Linux (<http://www.savoirfairelinux.com>).
 #
 #    This program is free software: you can redistribute it and/or modify
-#    it under the terms of the GNU General Public License as
+#    it under the terms of the GNU Affero General Public License as
 #    published by the Free Software Foundation, either version 3 of the
 #    License, or (at your option) any later version.
 #
 #    This program is distributed in the hope that it will be useful,
 #    but WITHOUT ANY WARRANTY; without even the implied warranty of
 #    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-#    GNU General Public License for more details.
+#    GNU Affero General Public License for more details.
 #
-#    You should have received a copy of the GNU General Public License
-#    along with this program.  If not, see <http://www.gnu.org/licenses/>.     
+#    You should have received a copy of the GNU Affero General Public License
+#    along with this program.  If not, see <http://www.gnu.org/licenses/>.  
 #
 ##############################################################################
 {
     "name" : "Management System",
-    "version" : "0.1",
+    "version" : "1.0",
     "author" : "Savoir-faire Linux",
     "website" : "http://www.savoirfairelinux.com",
+<<<<<<< 3bd4e08be18e0d712e45b8d242493aaf1167aec7
     "license" : "AGPL-3",
+=======
+    "license" : "AGPL",
+>>>>>>> [CHG] AGPL license; set verion to 1.0
     "category" : "Management System",
     "complexity" : "normal",
     "description": """\
