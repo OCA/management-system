c442e95a4f3a4193a95e67fadbb814969ce76221
Daniel Reis <dreis.pt@hotmail.com>
2012-12-17 16:19:36 +0000

    [CHG] AGPL license; set verion to 1.0

--- mgmtsystem_hazard/__openerp__.py
+++ mgmtsystem_hazard/__openerp__.py
@@ -5,29 +5,28 @@
 #    Copyright (C) 2010 Savoir-faire Linux (<http://www.savoirfairelinux.com>).
 #
 #    This program is free software: you can redistribute it and/or modify
-#    it under the terms of the GNU General Public License as
+#    it under the terms of the GNU Affero General Public License as
 #    published by the Free Software Foundation, either version 3 of the
 #    License, or (at your option) any later version.
 #
 #    This program is distributed in the hope that it will be useful,
 #    but WITHOUT ANY WARRANTY; without even the implied warranty of
 #    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-#    GNU General Public License for more details.
+#    GNU Affero General Public License for more details.
 #
-#    You should have received a copy of the GNU General Public License
-#    along with this program.  If not, see <http://www.gnu.org/licenses/>.     
+#    You should have received a copy of the GNU Affero General Public License
+#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #
 ##############################################################################
 {
     "name" : "Management System - Hazard",
-    "version" : "0.1",
+    "version" : "1.0",
     "author" : "Savoir-faire Linux",
     "website" : "http://www.savoirfairelinux.com",
     "license" : "AGPL-3",
     "category" : "Management System",
-    "description": """
-	This module enables you to manage the hazards and risks of your health and 
-        safety management system.	
+    "description": """\
+This module enables you to manage the hazards and risks of your health and safety management system.
     """,
     "depends" : [
         'mgmtsystem',
@@ -37,8 +36,8 @@
     "init_xml" : [],
     "update_xml" : [
         'security/ir.model.access.csv',
-	'mgmtsystem_hazard.xml',
-	'mgmtsystem_hazard_data.xml',
+        'mgmtsystem_hazard.xml',
+        'mgmtsystem_hazard_data.xml',
     ],
     "demo_xml" : [],
     "installable" : True,
