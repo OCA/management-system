fecb676d280677aba099447115aecd97beeae411
Daniel Reis <dreis.pt@hotmail.com>
2012-12-17 16:19:36 +0000

    [CHG] AGPL license; set verion to 1.0
    [FIX] .py files licenses to AGPL

--- mgmtsystem_survey/__openerp__.py
+++ mgmtsystem_survey/__openerp__.py
@@ -5,35 +5,38 @@
 #    Copyright (C) 2010 Savoir-faire Linux (<http://www.savoirfairelinux.com>).
 #
 #    This program is free software: you can redistribute it and/or modify
-#    it under the terms of the GNU General Public License as
+#    it under the terms of the GNU Affero General Public License as
 #    published by the Free Software Foundation, either version 3 of the
 #    License, or (at your option) any later version.
 #
 #    This program is distributed in the hope that it will be useful,
 #    but WITHOUT ANY WARRANTY; without even the implied warranty of
 #    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-#    GNU General Public License for more details.
+#    GNU Affero General Public License for more details.
 #
-#    You should have received a copy of the GNU General Public License
-#    along with this program.  If not, see <http://www.gnu.org/licenses/>.     
+#    You should have received a copy of the GNU Affero General Public License
+#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #
 ##############################################################################
 {
     "name" : "Management System - Survey",
-    "version" : "0.1",
+    "version" : "1.0",
     "author" : "Savoir-faire Linux",
     "website" : "http://www.savoirfairelinux.com",
+<<<<<<< 94d214c43b1c1025f4471291cd2a5e0bcb297021
+<<<<<<< 5df8409ead71a04a7588d51ad33d5d46bc319c79
     "license" : "AGPL-3",
+=======
+    "license" : "GPL",
+>>>>>>> [CHG] AGPL license; set verion to 1.0
+=======
+    "license" : "AGPL-3",
+>>>>>>> [CHG] Selections use words instead of letters; fixed AGPL-3 reference
     "category" : "Management System",
-    "description": """
-	This module enables you to manage your satisfaction surveys
-        and its answers.
-    """,
+    "description": """This module enables you to manage your satisfaction surveys and its answers.""",
     "depends" : ['survey'],
     "init_xml" : [],
-    "update_xml" : [
-	'mgmtsystem_survey.xml',
-    ],
+    "update_xml" : ['mgmtsystem_survey.xml',],
     "demo_xml" : [],
     "installable" : True,
     "certificate" : ''
