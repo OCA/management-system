69e2a8ae9fd9c5156878f4b27d893fadfdaf5f10
Virgil Dupras <virgil.dupras@savoirfairelinux.com>
2013-01-25 08:56:44 -0500

    [IMP+FIX] mgmtsystem_nonconformity: Migration scripts + several things
    [ADD] Added migration scripts for 0.1 --> 1.0 in the modules that needed it.
    
    mgmtsystem_nonconformity:
    
    * o --> open c --> done state change.
    * Migrating action relations to a many2many table.
    * Concatening action comments in a single field
    
    mgmtsystem_audit and mgmtsystem_review:
    
    * o --> open c --> done state change.
    
    [FIX] I had accidentally broken the nonconformity/action migration shortly before commit.
    
    [FIX] Fixed "Unknown field state for domain" errors.
    
    In 7.0, it's necessary, in a view, to have the field present in a view in order to use it
    in a domain. I added an invisible "state" field in relevant views to fix the errors.
    
    [CHG] Adjusted mgmgsystem_nonconformity to OpenERP 7.0.
    
    Like with mgmtsystem_action, the problem comes from changes made in OpenERP's messaging system.
    
    [FIX] Broken reference to wiki's group_id.
    
    In OpenERP 7, document_page had a parent_id instead of wiki's group_id. I've fixed the broken references that we had left in nonconformity and audit.
    
    [FIX] Fixed wrongful merge.
    
    The merge of nc-extend-7.0 was incorrect. It wrongly used the 6.1 version of mgmtsystem_audit.xml and mgmtsystem_nonconformity/mgmtsystem_nonconformity.xml. Fixed this.
    
    [CHG] Updated statusbar/buttons's layout to conform to OpenERP 7.0 style.
    
    Instead of a group at the bottom of the form, it's now a <header> tag at the top.
    
    Changed Actions, Audit, Nonconformity and Review.

---
+++ mgmtsystem_nonconformity/migrations/6.1.1.0/post-migration.py
@@ -0,0 +1,52 @@
+# -*- coding: utf-8 -*-
+##############################################################################
+#
+#    OpenERP, Open Source Management Solution
+#    This module copyright (C) 2013 Savoir-faire Linux (<http://www.savoirfairelinux.com>).
+#
+#    This program is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU Affero General Public License as
+#    published by the Free Software Foundation, either version 3 of the
+#    License, or (at your option) any later version.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU Affero General Public License for more details.
+#
+#    You should have received a copy of the GNU Affero General Public License
+#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#
+##############################################################################
+
+import os
+from osv import osv
+import logging
+
+logger = logging.getLogger('upgrade')
+
+def migrate(cr, version):
+    logger.info("Migrating mgmtsystem_nonconformity from version %s", version)
+    cr.execute("select count(*) from mgmtsystem_nonconformity_action_rel")
+    rowcount = cr.fetchone()[0]
+    if rowcount == 0:
+        logger.info("Moving nonconformity/action relations to mgmtsystem_nonconformity_action_rel")
+        for action_field in ('preventive_action_id', 'immediate_action_id', 'corrective_action_id'):
+            cr.execute("insert into mgmtsystem_nonconformity_action_rel"
+               "(nonconformity_id, action_id) "
+               "(SELECT id, %s FROM "
+               " mgmtsystem_nonconformity "
+               "WHERE %s IS NOT NULL )" % (action_field, action_field))
+    else:
+        logger.warning("Attempt to migrate nonconformity action IDs failed: migration was already done.")
+
+    logger.info("Concatening action comments into evaluation_comments")
+    cr.execute("update mgmtsystem_nonconformity set evaluation_comments = "
+           "effectiveness_preventive || ' ' || effectiveness_immediate || ' ' || effectiveness_corrective "
+           "where evaluation_comments is null")
+
+    logger.info("Updating state flags")
+    cr.execute("update mgmtsystem_nonconformity set state = 'open' where state = 'o'")
+    cr.execute("update mgmtsystem_nonconformity set state = 'done' where state = 'c'")
+
+    logger.info("mgmtsystem_nonconformity update... done!")
