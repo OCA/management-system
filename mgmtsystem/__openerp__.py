f1bc0903e9837b81e56a47bbe85d659efb249acb
Maxime Chambreuil <maxime.chambreuil@savoirfairelinux.com>
2013-04-13 12:51:13 -0400

    [IMP] Group inheritance
    [UPG] module manifest and views to v7 format
    
    [IMP] Add company_id in views
    
    [ADD] mgmtsystem_*_security.xml files
    
    [FIX] PEP8 compliance after running flake8

--- mgmtsystem/__openerp__.py
+++ mgmtsystem/__openerp__.py
@@ -1,6 +1,6 @@
 # -*- encoding: utf-8 -*-
 ##############################################################################
-#    
+#
 #    OpenERP, Open Source Management Solution
 #    Copyright (C) 2010 Savoir-faire Linux (<http://www.savoirfairelinux.com>).
 #
@@ -15,10 +15,11 @@
 #    GNU Affero General Public License for more details.
 #
 #    You should have received a copy of the GNU Affero General Public License
-#    along with this program.  If not, see <http://www.gnu.org/licenses/>.  
+#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #
 ##############################################################################
 {
+<<<<<<< 240068ec92e65feaead5d60c9ce38b82bb1dbb69
     "name" : "Management System",
     "version" : "1.0",
     "author" : "Savoir-faire Linux",
@@ -34,6 +35,15 @@
 >>>>>>> [CHG] Selections use words instead of letters; fixed AGPL-3 reference
     "category" : "Management System",
     "complexity" : "normal",
+=======
+    "name": "Management System",
+    "version": "1.0",
+    "author": "Savoir-faire Linux",
+    "website": "http://www.savoirfairelinux.com",
+    "license": "AGPL-3",
+    "category": "Management System",
+    "complexity": "normal",
+>>>>>>> [FIX] PEP8 compliance after running flake8
     "description": """\
 This module is the basis of any management system applications:
      * audit reports,
@@ -43,18 +53,20 @@ This module is the basis of any management system applications:
      * corrective actions,
      * improvement opportunities.
     """,
-    "depends" : ['base','board','document_page'],
-    "init_xml" : [],
-    "update_xml" : [
+    "depends": [
+        'base',
+        'board',
+        'document_page',
+    ],
+    "data": [
         'security/mgmtsystem_security.xml',
         'security/ir.model.access.csv',
         'mgmtsystem.xml',
         'mgmtsystem_system.xml',
         'board_mgmtsystem_view.xml',
     ],
-    "demo_xml" : [],
-    "installable" : True,
-    "certificate" : ''
+    "demo": [],
+    "installable": True,
+    "certificate": ''
 }
 # vim:expandtab:smartindent:tabstop=4:softtabstop=4:shiftwidth=4:
-
--- mgmtsystem/mgmtsystem_system.py
+++ mgmtsystem/mgmtsystem_system.py
@@ -1,6 +1,6 @@
 # -*- encoding: utf-8 -*-
 ##############################################################################
-#    
+#
 #    OpenERP, Open Source Management Solution
 #    Copyright (C) 2012 Savoir-faire Linux (<http://www.savoirfairelinux.com>).
 #
@@ -15,13 +15,14 @@
 #    GNU Affero General Public License for more details.
 #
 #    You should have received a copy of the GNU Affero General Public License
-#    along with this program.  If not, see <http://www.gnu.org/licenses/>.  
+#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #
 ##############################################################################
 
-from osv import fields, osv
+from osv import fields, orm
 
-class mgmtsystem_system(osv.osv):
+
+class mgmtsystem_system(orm.Model):
 
     _name = 'mgmtsystem.system'
     description = 'System'
@@ -35,10 +36,10 @@ class mgmtsystem_system(osv.osv):
         'manual': fields.many2one('document.page', 'Manual'),
 >>>>>>> [IMP] Update hooks with addons modules from v7
         'company_id': fields.many2one('res.company', 'Company')
-        }
+    }
 
     _defaults = {
         'company_id': lambda self, cr, uid, c: self.pool.get('res.users').browse(cr, uid, uid, c).company_id.id,
     }
 
-mgmtsystem_system()
+# vim:expandtab:smartindent:tabstop=4:softtabstop=4:shiftwidth=4:
