d903aa08fb62dfd5b0eaa36e17884c1085e40f84
Maxime Chambreuil <maxime.chambreuil@savoirfairelinux.com>
2013-08-29 15:45:45 -0400

    [FIX] PEP8 compliance after running flake8

--- mgmtsystem_audit/mgmtsystem_audit.py
+++ mgmtsystem_audit/mgmtsystem_audit.py
@@ -29,14 +29,14 @@ class mgmtsystem_audit(orm.Model):
         'name': fields.char('Name', size=50),
         'reference': fields.char('Reference', size=64, required=True, readonly=True),
         'date': fields.datetime('Date'),
-        'line_ids': fields.one2many('mgmtsystem.verification.line','audit_id','Verification List'),
-        'auditor_user_ids': fields.many2many('res.users','mgmtsystem_auditor_user_rel','user_id','mgmtsystem_audit_id','Auditors'),
-        'auditee_user_ids': fields.many2many('res.users','mgmtsystem_auditee_user_rel','user_id','mgmtsystem_audit_id','Auditees'),
+        'line_ids': fields.one2many('mgmtsystem.verification.line', 'audit_id', 'Verification List'),
+        'auditor_user_ids': fields.many2many('res.users', 'mgmtsystem_auditor_user_rel', 'user_id', 'mgmtsystem_audit_id', 'Auditors'),
+        'auditee_user_ids': fields.many2many('res.users', 'mgmtsystem_auditee_user_rel', 'user_id', 'mgmtsystem_audit_id', 'Auditees'),
         'strong_points': fields.text('Strong Points'),
         'to_improve_points': fields.text('Points To Improve'),
-        'imp_opp_ids': fields.many2many('mgmtsystem.action','mgmtsystem_audit_imp_opp_rel','mgmtsystem_action_id','mgmtsystem_audit_id','Improvement Opportunities'),
+        'imp_opp_ids': fields.many2many('mgmtsystem.action', 'mgmtsystem_audit_imp_opp_rel', 'mgmtsystem_action_id', 'mgmtsystem_audit_id', 'Improvement Opportunities'),
         'nonconformity_ids': fields.many2many('mgmtsystem.nonconformity', string='Nonconformities'),
-        'state': fields.selection([('open','Open'),('done','Closed')], 'State'),
+        'state': fields.selection([('open', 'Open'), ('done', 'Closed')], 'State'),
         'system_id': fields.many2one('mgmtsystem.system', 'System'),
         'company_id': fields.many2one('res.company', 'Company')
         }
@@ -61,7 +61,7 @@ class mgmtsystem_verification_line(orm.Model):
     _name = "mgmtsystem.verification.line"
     _description = "Verification Line"
     _columns = {
-        'name': fields.char('Question',size=300, required=True),
+        'name': fields.char('Question', size=300, required=True),
         'audit_id': fields.many2one('mgmtsystem.audit', 'Audit', ondelete='cascade', select=True),
 <<<<<<< ad4a1d15a9393eb931e2492c91792187aaf42bd0
         'procedure_id': fields.many2one('document.page', 'Procedure', ondelete='cascade', select=True),
