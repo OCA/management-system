ef1f91f6720a735391869ef0b3094d4816155ebe
Maxime Chambreuil <maxime.chambreuil@savoirfairelinux.com>
2013-08-29 15:45:45 -0400

    [FIX] PEP8 compliance after running flake8

--- document_page_environment_manual/__openerp__.py
+++ document_page_environment_manual/__openerp__.py
@@ -1,6 +1,6 @@
 # -*- encoding: utf-8 -*-
 ##############################################################################
-#    
+#
 #    OpenERP, Open Source Management Solution
 #    Copyright (C) 2010 Savoir-faire Linux (<http://www.savoirfairelinux.com>).
 #
@@ -15,23 +15,22 @@
 #    GNU General Public License for more details.
 #
 #    You should have received a copy of the GNU General Public License
-#    along with this program.  If not, see <http://www.gnu.org/licenses/>.     
+#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #
 ##############################################################################
 {
-    "name" : "Environment Manual",
-    "version" : "0.1",
-    "author" : "Savoir-faire Linux",
-    "website" : "http://www.savoirfairelinux.com",
-    "license" : "AGPL-3",
-    "category" : "Generic Modules/Others",
+    "name": "Document Management - Wiki - Environment Manual",
+    "version": "1.0",
+    "author": "Savoir-faire Linux",
+    "website": "http://www.savoirfairelinux.com",
+    "license": "AGPL-3",
+    "category": "Generic Modules/Others",
     "description": """Environment Manual Template
     """,
-    "depends" : ['mgmtsystem_manuals'],
-    "data" : ['document_page_environment_manual.xml'],
-    "demo" : [],
-    "installable" : True,
-    "certificate" : ''
+    "depends": ['mgmtsystem_manuals'],
+    "data": ['document_page_environment_manual.xml'],
+    "demo": [],
+    "installable": True,
+    "certificate": ''
 }
 # vim:expandtab:smartindent:tabstop=4:softtabstop=4:shiftwidth=4:
-
