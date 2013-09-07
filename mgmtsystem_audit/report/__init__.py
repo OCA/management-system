11afd801b84b6ce3a1072b7a7a111d6e3f0103df
Maxime Chambreuil <maxime.chambreuil@savoirfairelinux.com>
2013-09-07 16:50:11 -0400

    [FIX] PEP8 compliance in audit, action and nonconformity

--- mgmtsystem_audit/report/__init__.py
+++ mgmtsystem_audit/report/__init__.py
@@ -1,4 +1,9 @@
 # -*- encoding: utf-8 -*-
+<<<<<<< b2fef5464e02e345d90a60666de68bf7b7affe15
 import audit_report
 import verification_list
+=======
+from . import audit_report
+from . import verification_list
+>>>>>>> [FIX] PEP8 compliance in audit, action and nonconformity
 # vim:expandtab:smartindent:tabstop=4:softtabstop=4:shiftwidth=4:
