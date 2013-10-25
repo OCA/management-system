73876184aa7c1c41fcadd50ae1120101e4073faf
Sandy Carter <sandy.carter@savoirfairelinux.com>
2013-10-25 12:32:13 -0400

    [7.0.1.0 Migration scirpts] Added mgmtsystem_action. Made mgmtsystem_nonconformity system more robust.

--- mgmtsystem_nonconformity/migrations/7.0.1.0/post-migration.py
+++ mgmtsystem_nonconformity/migrations/7.0.1.0/post-migration.py
@@ -19,12 +19,55 @@
 #
 ##############################################################################
 
-import logging
-
-logger = logging.getLogger('upgrade')
+from openupgrade import openupgrade
 
 
+def migrate_nonconformity_action_ids(cr, column_names):
+    openupgrade.logged_query(cr,  """
+        SELECT COUNT(*)
+        FROM mgmtsystem_nonconformity_action_rel""")
+    if cr.fetchone()[0] > 0:
+        openupgrade.logger.warning(
+            "Attempt to migrate nonconformity action IDs failed: migration was already done.")
+        return
+    openupgrade.logger.info(
+        "Moving nonconformity/action relations to mgmtsystem_nonconformity_action_rel")
+    openupgrade.logged_query(cr, """
+        SELECT column_name
+        FROM information_schema.columns
+        WHERE table_name = 'mgmtsystem_nonconformity'""")
+    action_fields = ['preventive_action_id', 'immediate_action_id', 'corrective_action_id']
+    available_fields = [i for i in action_fields if i in column_names]
+    for action_field in available_fields:
+        openupgrade.logged_query(cr,  """
+            INSERT INTO mgmtsystem_nonconformity_action_rel (nonconformity_id, action_id)
+            (SELECT id, %s action_id FROM mgmtsystem_nonconformity
+             WHERE %s IS NOT NULL);""" % (action_field, action_field))
+
+
+def concatenate_action_comments(cr, column_names):
+    openupgrade.logger.info("Concatenating action comments into evaluation_comments")
+    action_fields = ['effectiveness_preventive', 'effectiveness_immediate', 'effectiveness_corrective']
+    concatenation = " || ' ' || ".join([i for i in action_fields if i in column_names])
+    if concatenation:
+        openupgrade.logged_query(cr,  """
+            UPDATE mgmtsystem_nonconformity
+            SET evaluation_comments = %s
+            WHERE evaluation_comments IS NULL;""" % concatenation)
+
+
+def update_state_flags(cr):
+    openupgrade.logger.info("Updating state flags")
+    for i in [('open', 'o'), ('done', 'c')]:
+        openupgrade.logged_query(cr,  """
+            UPDATE mgmtsystem_nonconformity
+            SET state = %s
+            WHERE state = %s;""", i)
+
+
+@openupgrade.migrate()
 def migrate(cr, version):
+<<<<<<< 44fdbb69bcd6ca9c8f04f08af740c84b8c036dc0
     logger.info("Migrating mgmtsystem_nonconformity from version %s", version)
     cr.execute("select count(*) from mgmtsystem_nonconformity_action_rel")
     rowcount = cr.fetchone()[0]
@@ -53,3 +96,13 @@ def migrate(cr, version):
     cr.execute("update mgmtsystem_nonconformity set state = 'done' where state = 'c'")
 
     logger.info("mgmtsystem_nonconformity update... done!")
+=======
+    openupgrade.logged_query(cr, """
+        SELECT column_name
+        FROM information_schema.columns
+        WHERE table_name = 'mgmtsystem_nonconformity'""")
+    column_names = (i[0] for i in cr.fetchall())
+    migrate_nonconformity_action_ids(cr, column_names)
+    concatenate_action_comments(cr, column_names)
+    update_state_flags(cr)
+>>>>>>> [7.0.1.0 Migration scirpts] Added mgmtsystem_action. Made mgmtsystem_nonconformity system more robust.
