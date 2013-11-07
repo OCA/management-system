f7df4c3bc8e52a9a65b175d54be806ffba82826a
Sandy Carter <sandy.carter@savoirfairelinux.com>
2013-11-07 17:30:25 -0500

    [FIX] Migration scripts no longer fully dependent on openupgrade

--- mgmtsystem_nonconformity/migrations/7.0.1.0/post-migration.py
+++ mgmtsystem_nonconformity/migrations/7.0.1.0/post-migration.py
@@ -19,54 +19,66 @@
 #
 ##############################################################################
 
-from openupgrade import openupgrade
+try:
+    from openupgrade.openupgrade import logged_query, logger
+except ImportError:
+    import logging
+    logger = logging.getLogger('upgrade')
+
+    def logged_query(cr, query, args=None):
+        if args is None:
+            args = []
+        res = cr.execute(query, args)
+        logger.debug('Running %s', query % tuple(args))
+        logger.debug('%s rows affected', cr.rowcount)
+        return cr.rowcount
 
 
 def migrate_nonconformity_action_ids(cr, column_names):
-    openupgrade.logged_query(cr,  """
+    logged_query(cr,  """
         SELECT COUNT(*)
         FROM mgmtsystem_nonconformity_action_rel""")
     if cr.fetchone()[0] > 0:
-        openupgrade.logger.warning(
+        logger.warning(
             "Attempt to migrate nonconformity action IDs failed: migration was already done.")
         return
-    openupgrade.logger.info(
+    logger.info(
         "Moving nonconformity/action relations to mgmtsystem_nonconformity_action_rel")
-    openupgrade.logged_query(cr, """
+    logged_query(cr, """
         SELECT column_name
         FROM information_schema.columns
         WHERE table_name = 'mgmtsystem_nonconformity'""")
     action_fields = ['preventive_action_id', 'immediate_action_id', 'corrective_action_id']
     available_fields = [i for i in action_fields if i in column_names]
     for action_field in available_fields:
-        openupgrade.logged_query(cr,  """
+        logged_query(cr,  """
             INSERT INTO mgmtsystem_nonconformity_action_rel (nonconformity_id, action_id)
             (SELECT id, %s action_id FROM mgmtsystem_nonconformity
              WHERE %s IS NOT NULL);""" % (action_field, action_field))
 
 
 def concatenate_action_comments(cr, column_names):
-    openupgrade.logger.info("Concatenating action comments into evaluation_comments")
+    logger.info("Concatenating action comments into evaluation_comments")
     action_fields = ['effectiveness_preventive', 'effectiveness_immediate', 'effectiveness_corrective']
     concatenation = " || ' ' || ".join([i for i in action_fields if i in column_names])
     if concatenation:
-        openupgrade.logged_query(cr,  """
+        logged_query(cr,  """
             UPDATE mgmtsystem_nonconformity
             SET evaluation_comments = %s
             WHERE evaluation_comments IS NULL;""" % concatenation)
 
 
 def update_state_flags(cr):
-    openupgrade.logger.info("Updating state flags")
+    logger.info("Updating state flags")
     for i in [('open', 'o'), ('done', 'c')]:
-        openupgrade.logged_query(cr,  """
+        logged_query(cr,  """
             UPDATE mgmtsystem_nonconformity
             SET state = %s
             WHERE state = %s;""", i)
 
 
-@openupgrade.migrate()
 def migrate(cr, version):
+<<<<<<< d1ae6c91b0f02e30eb83ceb60d42bbf891243ce1
 <<<<<<< 44fdbb69bcd6ca9c8f04f08af740c84b8c036dc0
     logger.info("Migrating mgmtsystem_nonconformity from version %s", version)
     cr.execute("select count(*) from mgmtsystem_nonconformity_action_rel")
@@ -98,6 +110,9 @@ def migrate(cr, version):
     logger.info("mgmtsystem_nonconformity update... done!")
 =======
     openupgrade.logged_query(cr, """
+=======
+    logged_query(cr, """
+>>>>>>> [FIX] Migration scripts no longer fully dependent on openupgrade
         SELECT column_name
         FROM information_schema.columns
         WHERE table_name = 'mgmtsystem_nonconformity'""")
