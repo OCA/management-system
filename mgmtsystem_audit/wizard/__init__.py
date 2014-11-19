834d2f8162872a529ff201360f880d40bd410013
Loïc Faure-Lacroix <lamerstar@gmail.com>
2014-11-19 17:55:26 +0300

    Moved mgmtsystem_audit to root and fixed imports

--- mgmtsystem_audit/wizard/__init__.py
+++ mgmtsystem_audit/wizard/__init__.py
@@ -1,3 +1,7 @@
 # -*- encoding: utf-8 -*-
+<<<<<<< 697b7c1967849d398f6212cef8d15618f8ce3201
 from . import copy_verification_lines
+=======
+import copy_verification_lines
+>>>>>>> Moved mgmtsystem_audit to root and fixed imports
 # vim:expandtab:smartindent:tabstop=4:softtabstop=4:shiftwidth=4:
