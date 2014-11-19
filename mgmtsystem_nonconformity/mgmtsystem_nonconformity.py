64060a4f39d5a449e38c46b81f9c5b47db3d258f
Loïc Faure-Lacroix <lamerstar@gmail.com>
2014-11-19 15:09:24 +0300

    Use stage_id for actions

--- mgmtsystem_nonconformity/mgmtsystem_nonconformity.py
+++ mgmtsystem_nonconformity/mgmtsystem_nonconformity.py
@@ -558,16 +558,20 @@ class mgmtsystem_nonconformity(orm.Model):
             )
         self.case_open_send_note(cr, uid, ids, context=context)
         # Open related Actions
+<<<<<<< 6794e3612a1105b8d45a05fbe2bd17eca30cd25f
 <<<<<<< b7b2c1a83a414b319605200700d3b02067ba784d
 >>>>>>> Moved mgmtsystem_nonconformity to root for port
         if o.immediate_action_id and o.immediate_action_id.state == 'draft':
 =======
         # TODO static variables... hmm
+=======
+        # TODO static variables... hmm update state isn't going to work
+>>>>>>> Use stage_id for actions
         if o.immediate_action_id and o.immediate_action_id.stage_id.name.lower() == 'draft':
 >>>>>>> Small fix to use stage_id.name instead of state
             o.immediate_action_id.case_open()
         for a in o.action_ids:
-            if a.state == 'draft':
+            if a.stage_id.name.lower() == 'draft':
                 a.case_open()
 <<<<<<< 8a12276cf0affae66506dcba67980c75aac42247
         return self.write(cr, uid, ids, {'state': 'open', 'evaluation_date': None, 'evaluation_user_id': None}, context=context)
@@ -619,12 +623,12 @@ class mgmtsystem_nonconformity(orm.Model):
             raise orm.except_orm(_('Error !'), _('Effectiveness evaluation must be performed before closing.'))
 =======
         if (o.immediate_action_id
-                and o.immediate_action_id.state not in done_states):
+                and o.immediate_action_id.stage_id.name.lower() not in done_states):
             raise orm.except_orm(
                 _('Error !'),
                 _('Immediate action from analysis has not been closed.')
             )
-        if ([i for i in o.action_ids if i.state not in done_states]):
+        if ([i for i in o.action_ids if i.stage_id.name.lower() not in done_states]):
             raise orm.except_orm(
                 _('Error !'),
                 _('Not all actions have been closed.')
