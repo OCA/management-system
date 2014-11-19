5327081958707d0e271b3cf8d5e021577b9b46c6
Loïc Faure-Lacroix <lamerstar@gmail.com>
2014-11-19 10:58:28 +0300

    [MIG] mgmtsystem_nonconformity: Migration to 8.0
    Updated module as installable and removed depdencie on base_status
    
    See
    
    the base_status module is apparently gone so we have to work without it.
    https://code.launchpad.net/~openerp-dev/openobject-addons/trunk-remove-base_status-tde/+merge/179887
    
    Small fix to use stage_id.name instead of state
    
    State doesn't exist anymore
    
    Use stage_id for actions
    
    Add support for settled, rejected instead of done/canceled... quite ugly
    
    Separated python in each model file
    
    Added missing methodcase_close_send_note for workflows
    
    Updated model severity to V8
    
    Port cause model to v8
    
    Fix flake8 error
    
    Fix small error in `cause` and upgraded `origin` to v8
    
    Splitted xml views into 5 different xml files
    
    Moved icon.png to static/description
    
    Added tests and updated cause to v8
    
    Added test origin and changed name of test class for cause
    
    Removed severity redefined here
    
    Added tests for create nonconformity
    
    Ported fields to v8
    
    Fix indent block
    
    Removed ID and removed named lambdas
    
    Simplify the name_get method
    
    Get exceptions without importing orm
    
    Removed size
    
    Simplified name_get and removed id
    
    Replace try except by self.assertRaise
    
    Added group in forms
    
    Reformat xml files removed tabs/space mix and 2 space indent
    
    Fix tests
    
    Added left and right to cause and origin
    
    Add ondelete to parent_id
    
    Fix tests
    
    Improved name_get method and full coverage or origin/cause
    
    Added workflow test
    
    Added missing copyrights
    
    Fix flake errors
    
    Fix more flake errors
    
    Some actions can only be done once actions, analysis
    
    Fix even more flakes errors
    
    Test reset button
    
    Added missing fields (used in demo data)
    
    Test state nonconformity name
    
    Remove states as we are using stages on actions...
    
    It would make more sense to add a property to stages that mark some
    stages as ending stages instead of checking by names.
    
    Use stage is ending and is starting instead of by name
    
    Test analysis instead of evaluation
    
    Fix flake8 errors
    
    Added base security
    
    Security rule in wrong project

--- mgmtsystem_nonconformity/tests/__init__.py
+++ mgmtsystem_nonconformity/tests/__init__.py
@@ -1,20 +1,8 @@
-<<<<<<< 06aa4f2b70a00af752d29b3a543ef5ebd33e203c
-# -*- encoding: utf-8 -*-
-<<<<<<< 8a12276cf0affae66506dcba67980c75aac42247
-import mgmtsystem_nonconformity
-# vim:expandtab:smartindent:tabstop=4:softtabstop=4:shiftwidth=4:
-=======
-
-<<<<<<< 75bff4df4701f5ed835e70fe7acf4ab910086576
-from . import mgmtsystem_nonconformity
->>>>>>> Moved mgmtsystem_nonconformity to root for port
-=======
-=======
 # -*- coding: utf-8 -*-
 ##############################################################################
 #
 #    OpenERP, Open Source Management Solution
-#    Copyright (C) 2013 Savoir-faire Linux (<http://www.savoirfairelinux.com>).
+#    Copyright (C) 2010 Savoir-faire Linux (<http://www.savoirfairelinux.com>).
 #
 #    This program is free software: you can redistribute it and/or modify
 #    it under the terms of the GNU Affero General Public License as
@@ -30,6 +18,9 @@ from . import mgmtsystem_nonconformity
 #    along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #
 ##############################################################################
->>>>>>> Added missing copyrights
-from . import models
->>>>>>> Separated python in each model file
+from . import (
+    test_cause,
+    test_origin,
+    test_nonconformity,
+    test_workflows,
+)
