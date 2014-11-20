d2752816ba8f7dadbf353f1e83decafcc22eca5a
Loïc Faure-Lacroix <lamerstar@gmail.com>
2014-11-20 13:46:06 +0300

    Converted imports to relative import to prevent namespace conflict

--- mgmtsystem_audit/__init__.py
+++ mgmtsystem_audit/__init__.py
@@ -1,4 +1,5 @@
 # -*- encoding: utf-8 -*-
+<<<<<<< a388c9b0a4cc1dd786ea7063756aefdf4864657a
 <<<<<<< 697b7c1967849d398f6212cef8d15618f8ce3201
 from . import mgmtsystem_audit
 from . import report
@@ -8,4 +9,9 @@ import mgmtsystem_audit
 import report
 import wizard
 >>>>>>> Moved mgmtsystem_audit to root and fixed imports
+=======
+from . import mgmtsystem_audit
+from . import report
+from . import wizard
+>>>>>>> Converted imports to relative import to prevent namespace conflict
 # vim:expandtab:smartindent:tabstop=4:softtabstop=4:shiftwidth=4:
--- mgmtsystem_audit/wizard/__init__.py
+++ mgmtsystem_audit/wizard/__init__.py
@@ -1,7 +1,11 @@
 # -*- encoding: utf-8 -*-
+<<<<<<< a388c9b0a4cc1dd786ea7063756aefdf4864657a
 <<<<<<< 697b7c1967849d398f6212cef8d15618f8ce3201
 from . import copy_verification_lines
 =======
 import copy_verification_lines
 >>>>>>> Moved mgmtsystem_audit to root and fixed imports
+=======
+from . import copy_verification_lines
+>>>>>>> Converted imports to relative import to prevent namespace conflict
 # vim:expandtab:smartindent:tabstop=4:softtabstop=4:shiftwidth=4:
