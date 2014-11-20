9859f44ae2f3a6b47a9d62e6c7f1a703d92aedf6
Loïc Faure-Lacroix <lamerstar@gmail.com>
2014-11-20 13:46:06 +0300

    Converted imports to relative import to prevent namespace conflict

--- mgmtsystem_audit/report/__init__.py
+++ mgmtsystem_audit/report/__init__.py
@@ -1,4 +1,5 @@
 # -*- encoding: utf-8 -*-
+<<<<<<< a388c9b0a4cc1dd786ea7063756aefdf4864657a
 <<<<<<< 697b7c1967849d398f6212cef8d15618f8ce3201
 <<<<<<< b2fef5464e02e345d90a60666de68bf7b7affe15
 import audit_report
@@ -11,4 +12,8 @@ from . import verification_list
 import audit_report
 import verification_list
 >>>>>>> Moved mgmtsystem_audit to root and fixed imports
+=======
+from . import audit_report
+from . import verification_list
+>>>>>>> Converted imports to relative import to prevent namespace conflict
 # vim:expandtab:smartindent:tabstop=4:softtabstop=4:shiftwidth=4:
