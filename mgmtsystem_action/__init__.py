d3e2f444c0ddd9b043ff0fba27d784812d36d210
Loïc Faure-Lacroix <lamerstar@gmail.com>
2014-11-20 15:47:24 +0300

    Ported mgmtsystem_action
    Fix typo and pep8
    
    Changes made for API v8
    
    Removed pdb lines
    
    Removed vim lines and fixed domain
    
    Removed comments and commented code
    
    Added tests and updated code to v8
    
    Fix flake8 errors
    
    Updated according to reviews
    
    Removed case_close
    
    It is used and only contains things from mgmtsystem_nonconformity
    
    Removed blank line, flake8 not happy
    
    Removed check for user_id as user_id cannot be null
    
    When doing `case.user_id = None`, it will throw this error:
    
        IntegrityError: null value in column "partner_id" violates not-null
        constraint
    
    Remove transition case_close and moved icon

--- mgmtsystem_action/__init__.py
+++ mgmtsystem_action/__init__.py
@@ -1,3 +1,8 @@
 # -*- encoding: utf-8 -*-
+<<<<<<< 2cb3e23cd6da406a2afd4eedfd7745ab01746e88
 import mgmtsystem_action
 # vim:expandtab:smartindent:tabstop=4:softtabstop=4:shiftwidth=4:
+=======
+
+from . import mgmtsystem_action
+>>>>>>> Ported mgmtsystem_action
