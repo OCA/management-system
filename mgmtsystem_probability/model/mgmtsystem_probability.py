b33462dab6d739efb8bad01a147b726b50bdf085
Loïc Faure-Lacroix <lamerstar@gmail.com>
2015-02-10 15:33:26 +0300

    Ported module mgmtsystem_severity to mgmtsystem_probability

---
+++ mgmtsystem_probability/model/mgmtsystem_probability.py
@@ -0,0 +1,47 @@
+# -*- encoding: utf-8 -*-
+##############################################################################
+#
+#    OpenERP, Open Source Management Solution
+#    Copyright (C) 2015 - Present
+#    Savoir-faire Linux (<http://www.savoirfairelinux.com>).
+#
+#    This program is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU Affero General Public License as
+#    published by the Free Software Foundation, either version 3 of the
+#    License, or (at your option) any later version.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU Affero General Public License for more details.
+#
+#    You should have received a copy of the GNU Affero General Public License
+#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#
+##############################################################################
+from openerp.tools.translate import _
+from osv import fields, orm
+
+_CATEGORIES = [
+    ("hazard", _("hazard")),
+    ("security", _("security")),
+]
+
+
+class MgmtSystemProbability(orm.Model):
+
+    """
+    Define the Probability for management system.
+
+    Allow you to manage scale of probability (or likelihood)
+    used across different modules (health and safety, information security).
+    """
+
+    _name = "mgmtsystem.probability"
+    description = "Management System Probability"
+
+    _columns = {
+        "name": fields.char("Name", required=True),
+        "value": fields.integer("Value", required=True),
+        "category": fields.selection(_CATEGORIES, "Category", required=True),
+    }
---
+++ mgmtsystem_probability/tests/test_create_probability.py
@@ -0,0 +1,72 @@
+from openerp.tests.common import TransactionCase
+from psycopg2 import IntegrityError
+
+
+class TestCreateProbability(TransactionCase):
+
+    """
+    Test management probability object.
+
+    Test the management probability object creation.
+    It checks that each fields are required and that
+    a valid value creates an entry.
+    """
+
+
+    def setUp(self):
+        super(TestCreateProbability, self).setUp()
+
+        self.probabilities = self.registry('mgmtsystem.probability')
+
+    def test_create_probability(self):
+        id = self.probabilities.create(self.cr, self.uid, {
+            "name": "test",
+            "value": 0,
+            "category": "hazard"
+        })
+
+        self.assertNotEqual(id, 0)
+
+        obj = self.probabilities.browse(self.cr, self.uid, id)
+
+        self.assertEqual(obj.value, 0)
+        self.assertEqual(obj.name, "test")
+        self.assertEqual(obj.category, "hazard")
+
+        id = self.probabilities.create(self.cr, self.uid, {
+            "name": "test2",
+            "value": 10,
+            "category": "security"
+        })
+
+        self.assertNotEqual(id, 0)
+
+        obj = self.probabilities.browse(self.cr, self.uid, id)
+
+        self.assertEqual(obj.value, 10)
+        self.assertEqual(obj.name, "test2")
+        self.assertEqual(obj.category, "security")
+
+    def test_create_probability_without_name(self):
+
+        with self.assertRaises(IntegrityError):
+            self.probabilities.create(self.cr, self.uid, {
+                "value": 0,
+                "category": "hazard"
+            })
+
+    def test_create_probability_without_value(self):
+
+        with self.assertRaises(IntegrityError):
+            self.probabilities.create(self.cr, self.uid, {
+                "name": "test",
+                "category": "hazard"
+            })
+
+    def test_create_probability_without_category(self):
+
+        with self.assertRaises(IntegrityError):
+            self.probabilities.create(self.cr, self.uid, {
+                "name": "test",
+                "value": 0,
+            })
