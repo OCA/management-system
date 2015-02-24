ca602e4eba160fb077a69eb835476ba5711979ec
Loïc Faure-Lacroix <lamerstar@gmail.com>
2015-02-24 15:05:46 +0300

    Prevent ghosting id and better name for severity model

--- mgmtsystem_severity/tests/test_create_severity.py
+++ mgmtsystem_severity/tests/test_create_severity.py
@@ -15,32 +15,32 @@ class TestCreateSeverity(TransactionCase):
     def setUp(self):
         super(TestCreateSeverity, self).setUp()
 
-        self.severities = self.registry('mgmtsystem.severity')
+        self.severity_model = self.registry('mgmtsystem.severity')
 
     def test_create_severity(self):
-        id = self.severities.create(self.cr, self.uid, {
+        severity_id = self.severity_model.create(self.cr, self.uid, {
             "name": "test",
             "value": 0,
             "category": "hazard"
         })
 
-        self.assertNotEqual(id, 0)
+        self.assertNotEqual(severity_id, 0)
 
-        obj = self.severities.browse(self.cr, self.uid, id)
+        obj = self.severity_model.browse(self.cr, self.uid, severity_id)
 
         self.assertEqual(obj.value, 0)
         self.assertEqual(obj.name, "test")
         self.assertEqual(obj.category, "hazard")
 
-        id = self.severities.create(self.cr, self.uid, {
+        severity_id = self.severity_model.create(self.cr, self.uid, {
             "name": "test2",
             "value": 10,
             "category": "security"
         })
 
-        self.assertNotEqual(id, 0)
+        self.assertNotEqual(severity_id, 0)
 
-        obj = self.severities.browse(self.cr, self.uid, id)
+        obj = self.severity_model.browse(self.cr, self.uid, severity_id)
 
         self.assertEqual(obj.value, 10)
         self.assertEqual(obj.name, "test2")
@@ -49,7 +49,7 @@ class TestCreateSeverity(TransactionCase):
     def test_create_serverity_without_name(self):
 
         with self.assertRaises(IntegrityError):
-            self.severities.create(self.cr, self.uid, {
+            self.severity_model.create(self.cr, self.uid, {
                 "value": 0,
                 "category": "hazard"
             })
@@ -57,7 +57,7 @@ class TestCreateSeverity(TransactionCase):
     def test_create_serverity_without_value(self):
 
         with self.assertRaises(IntegrityError):
-            self.severities.create(self.cr, self.uid, {
+            self.severity_model.create(self.cr, self.uid, {
                 "name": "test",
                 "category": "hazard"
             })
@@ -65,7 +65,7 @@ class TestCreateSeverity(TransactionCase):
     def test_create_serverity_without_category(self):
 
         with self.assertRaises(IntegrityError):
-            self.severities.create(self.cr, self.uid, {
+            self.severity_model.create(self.cr, self.uid, {
                 "name": "test",
                 "value": 0,
             })
