69ef027ca030aca72bb5588196ad88aa14bea2d6
Sandy Carter <sandy.carter@savoirfairelinux.com>
2015-02-27 15:11:08 -0500

    Update translations
    Export pot and update po files
    Reduce a translation which included unneeded html

--- mgmtsystem_nonconformity/model/mgmtsystem_nonconformity.py
+++ mgmtsystem_nonconformity/model/mgmtsystem_nonconformity.py
@@ -201,10 +201,10 @@ class MgmtsystemNonconformity(base_state, orm.Model):
             msg = '%s <b>%s</b>' % (pre, text)
             if data:
                 o = self.browse(cr, uid, ids, context=context)[0]
-                post = _('''
+                post = '''
 <br />
-<ul><li> <b>Stage:</b> %s \xe2\x86\x92 %s</li></ul>\
-''') % (o.state, data['state'])
+<ul><li> <b>''' + _('Stage:') + '''</b> %s \xe2\x86\x92 %s</li></ul>\
+''' % (o.state, data['state'])
                 msg += post
             self.message_post(cr, uid, [id], body=msg, context=context)
         return True
