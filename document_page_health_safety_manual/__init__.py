5601acd032f1befdf7038cd6ecd483cba84806ed
Loïc Faure-Lacroix <lamerstar@gmail.com>
2015-03-24 22:43:39 +0300

    Add README.rst and move icon.png to description

--- document_page_health_safety_manual/__init__.py
+++ document_page_health_safety_manual/__init__.py
@@ -1,17 +1,26 @@
 # -*- encoding: utf-8 -*-
 ##############################################################################
+<<<<<<< HEAD
 #    
+=======
+#
+>>>>>>> Add README.rst and move icon.png to description
 #    OpenERP, Open Source Management Solution
 #    Copyright (C) 2010 Savoir-faire Linux (<http://www.savoirfairelinux.com>).
 #
 #    This program is free software: you can redistribute it and/or modify
+<<<<<<< HEAD
 #    it under the terms of the GNU General Public License as
+=======
+#    it under the terms of the GNU Affero General Public License as
+>>>>>>> Add README.rst and move icon.png to description
 #    published by the Free Software Foundation, either version 3 of the
 #    License, or (at your option) any later version.
 #
 #    This program is distributed in the hope that it will be useful,
 #    but WITHOUT ANY WARRANTY; without even the implied warranty of
 #    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+<<<<<<< HEAD
 #    GNU General Public License for more details.
 #
 #    You should have received a copy of the GNU General Public License
@@ -19,3 +28,11 @@
 #
 ##############################################################################
 # vim:expandtab:smartindent:tabstop=4:softtabstop=4:shiftwidth=4:
+=======
+#    GNU Affero General Public License for more details.
+#
+#    You should have received a copy of the GNU Affero General Public License
+#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#
+##############################################################################
+>>>>>>> Add README.rst and move icon.png to description
