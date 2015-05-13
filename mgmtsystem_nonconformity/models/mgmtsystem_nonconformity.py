a9632cc296548478645ec580acf1ebd6278cc0e1
Sandy Carter <sandy.carter@savoirfairelinux.com>
2015-05-13 13:34:53 -0400

    Cleanup and full use of v8 and track features

--- mgmtsystem_nonconformity/models/mgmtsystem_nonconformity.py
+++ mgmtsystem_nonconformity/models/mgmtsystem_nonconformity.py
@@ -18,6 +18,7 @@
 #    along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #
 ##############################################################################
+<<<<<<< c2538af8ebe5dc74b251cb209e1c58e9c4d99446
 <<<<<<< 06aa4f2b70a00af752d29b3a543ef5ebd33e203c
 
 <<<<<<< 8a12276cf0affae66506dcba67980c75aac42247
@@ -36,6 +37,10 @@ from openerp.tools.translate import _
 from openerp import netsvc
 from openerp.exceptions import except_orm
 from openerp import models, api, fields
+=======
+
+from openerp import models, api, fields, netsvc, exceptions, _
+>>>>>>> Cleanup and full use of v8 and track features
 
 from openerp.tools import (
     DEFAULT_SERVER_DATETIME_FORMAT as DATETIME_FORMAT,
@@ -208,7 +213,7 @@ _STATES_DICT = dict(_STATES)
 >>>>>>> Moved mgmtsystem_nonconformity to root for port
 
 
-class mgmtsystem_nonconformity(models.Model):
+class MgmtsystemNonconformity(models.Model):
 
     _name = "mgmtsystem.nonconformity"
     _description = "Nonconformity"
@@ -216,14 +221,14 @@ class mgmtsystem_nonconformity(models.Model):
     _inherit = ['mail.thread']
     _order = "date desc"
     _track = {
-       'field': {
-           'mgmtsystem_nonconformity.subtype_analysis': (
-               lambda s, c, u, o, ctx=None: o["state"] == "analysis"
-           ),
-           'mgmtsystem_nonconformity.subtype_pending': (
-               lambda s, c, u, o, ctx=None: o["state"] == "pending"
-           ),
-       },
+        'field': {
+            'mgmtsystem_nonconformity.subtype_analysis': (
+                lambda s, c, u, o, ctx=None: o["state"] == "analysis"
+            ),
+            'mgmtsystem_nonconformity.subtype_pending': (
+                lambda s, c, u, o, ctx=None: o["state"] == "pending"
+            ),
+        },
     }
 
     def _state_name(self):
@@ -533,6 +538,11 @@ class mgmtsystem_nonconformity(models.Model):
         domain="[('nonconformity_id', '=', id)]",
     )
 
+    @property
+    @api.multi
+    def verbose_name(self):
+        return self.env['ir.model'].search([('model', '=', self._name)]).name
+
     @api.model
     def create(self, vals):
         vals.update({
@@ -555,26 +565,25 @@ class mgmtsystem_nonconformity(models.Model):
             'ref': self.env['ir.sequence'].get('mgmtsystem.nonconformity')
 >>>>>>> Added tests for create nonconformity
         })
-        return super(mgmtsystem_nonconformity, self).create(vals)
+        return super(MgmtsystemNonconformity, self).create(vals)
 
-    def message_auto_subscribe(
-            self, cr, uid, ids, updated_fields, context=None, values=None):
-        """Add the reponsible, manager and OpenChatter follow list."""
-        o = self.browse(cr, uid, ids, context=context)[0]
+    @api.multi
+    def message_auto_subscribe(self, updated_fields, values=None):
+        """Add the responsible, manager and OpenChatter follow list."""
+        self.ensure_one()
         user_ids = [
-            o.responsible_user_id.id,
-            o.manager_user_id.id,
-            o.author_user_id.id,
+            self.responsible_user_id.id,
+            self.manager_user_id.id,
+            self.author_user_id.id,
         ]
-        self.message_subscribe_users(
-            cr, uid, ids, user_ids=user_ids, subtype_ids=None, context=context
-        )
-        return super(mgmtsystem_nonconformity, self).message_auto_subscribe(
-            cr, uid, ids, updated_fields=updated_fields, context=context,
+        self.message_subscribe_users(user_ids=user_ids, subtype_ids=None)
+        return super(MgmtsystemNonconformity, self).message_auto_subscribe(
+            updated_fields=updated_fields,
             values=values
         )
 >>>>>>> Moved mgmtsystem_nonconformity to root for port
 
+<<<<<<< c2538af8ebe5dc74b251cb209e1c58e9c4d99446
 <<<<<<< 2024a34797b9dc184b846f5ee3e9771d37c76175
     def case_send_note(self, cr, uid, ids, text, data=None, context=None):
         for id in ids:
@@ -604,11 +613,16 @@ class mgmtsystem_nonconformity(models.Model):
 =======
 >>>>>>> Use track and mail message subtypes instead of overriding functions
     def wkf_analysis(self, cr, uid, ids, context=None):
+=======
+    @api.multi
+    def wkf_analysis(self):
+>>>>>>> Cleanup and full use of v8 and track features
         """Change state from draft to analysis"""
-        data = {
+        return self.write({
             'state': 'analysis',
             'analysis_date': None,
             'analysis_user_id': None}
+<<<<<<< c2538af8ebe5dc74b251cb209e1c58e9c4d99446
 <<<<<<< 2024a34797b9dc184b846f5ee3e9771d37c76175
 <<<<<<< 8a12276cf0affae66506dcba67980c75aac42247
         self.case_send_note(cr, uid, ids, _('Analysis'), data=data, context=context)
@@ -620,9 +634,14 @@ class mgmtsystem_nonconformity(models.Model):
 =======
 >>>>>>> Use track and mail message subtypes instead of overriding functions
         return self.write(cr, uid, ids, data, context=context)
+=======
+        )
+>>>>>>> Cleanup and full use of v8 and track features
 
-    def action_sign_analysis(self, cr, uid, ids, context=None):
+    @api.multi
+    def action_sign_analysis(self):
         """Sign-off the analysis"""
+<<<<<<< c2538af8ebe5dc74b251cb209e1c58e9c4d99446
         o = self.browse(cr, uid, ids, context=context)[0]
         if o.state != 'analysis':
 <<<<<<< c78ad3ce9f0ea5ee98baa923b2662899cec0c8cc
@@ -639,41 +658,53 @@ class mgmtsystem_nonconformity(models.Model):
             raise except_orm(
 >>>>>>> Get exceptions without importing orm
                 _('Error !'),
+=======
+        self.ensure_one()
+        if self.state != 'analysis':
+            raise exceptions.ValidationError(
+>>>>>>> Cleanup and full use of v8 and track features
                 _('This action can only be done in the Analysis state.')
             )
-        if o.analysis_date:
-            raise except_orm(
-                _('Error !'),
+        if self.analysis_date:
+            raise exceptions.ValidationError(
                 _('Analysis is already approved.')
             )
-        if not o.analysis:
-            raise except_orm(
-                _('Error !'),
+        if not self.analysis:
+            raise exceptions.ValidationError(
                 _('Please provide an analysis before approving.')
             )
-        vals = {
+        self.write({
             'analysis_date': time.strftime(DATETIME_FORMAT),
+<<<<<<< c2538af8ebe5dc74b251cb209e1c58e9c4d99446
             'analysis_user_id': uid,
         }
 >>>>>>> Moved mgmtsystem_nonconformity to root for port
         self.write(cr, uid, ids, vals, context=context)
         msg = '%s <b>%s</b>' % (self._description, _('Analysis Approved'))
         self.message_post(cr, uid, ids, body=msg, context=context)
+=======
+            'analysis_user_id': self._uid,
+        })
+        self.message_post(
+            body='%s <b>%s</b>' % (self.verbose_name, _('Analysis Approved'))
+        )
+>>>>>>> Cleanup and full use of v8 and track features
         return True
 
-    def wkf_review(self, cr, uid, ids, context=None):
+    @api.multi
+    def wkf_review(self):
         """Change state from analysis to pending approval"""
-        for o in self.browse(cr, uid, ids, context=context):
+        for o in self:
             if not o.analysis_date:
-                raise except_orm(
-                    _('Error!'),
-                    _('Analysis must be performed before submiting to '
+                raise exceptions.ValidationError(
+                    _('Analysis must be performed before submitting to '
                       'approval.')
                 )
-        vals = {
+        return self.write({
             'state': 'pending',
             'actions_date': None,
             'actions_user_id': None}
+<<<<<<< c2538af8ebe5dc74b251cb209e1c58e9c4d99446
 <<<<<<< 2024a34797b9dc184b846f5ee3e9771d37c76175
 <<<<<<< 8a12276cf0affae66506dcba67980c75aac42247
         self.case_send_note(cr, uid, ids, _('Pending Approval'), data=vals, context=context)
@@ -685,9 +716,14 @@ class mgmtsystem_nonconformity(models.Model):
 =======
 >>>>>>> Use track and mail message subtypes instead of overriding functions
         return self.write(cr, uid, ids, vals, context=context)
+=======
+        )
+>>>>>>> Cleanup and full use of v8 and track features
 
-    def action_sign_actions(self, cr, uid, ids, context=None):
+    @api.multi
+    def action_sign_actions(self):
         """Sign-off the action plan"""
+<<<<<<< c2538af8ebe5dc74b251cb209e1c58e9c4d99446
         o = self.browse(cr, uid, ids, context=context)[0]
         if o.state != 'pending':
 <<<<<<< c78ad3ce9f0ea5ee98baa923b2662899cec0c8cc
@@ -704,21 +740,25 @@ class mgmtsystem_nonconformity(models.Model):
             raise except_orm(
 >>>>>>> Get exceptions without importing orm
                 _('Error !'),
+=======
+        self.ensure_one()
+        if self.state != 'pending':
+            raise exceptions.ValidationError(
+>>>>>>> Cleanup and full use of v8 and track features
                 _('This action can only be done in the Pending for Approval '
                   'state.')
             )
-        if o.actions_date:
-            raise except_orm(
-                _('Error !'),
+        if self.actions_date:
+            raise exceptions.ValidationError(
                 _('Action plan is already approved.')
             )
-        if not self.browse(cr, uid, ids, context=context)[0].analysis_date:
-            raise except_orm(
-                _('Error !'),
+        if not self.analysis_date:
+            raise exceptions.ValidationError(
                 _('Analysis approved before the review confirmation.')
             )
-        vals = {
+        self.write({
             'actions_date': time.strftime(DATETIME_FORMAT),
+<<<<<<< c2538af8ebe5dc74b251cb209e1c58e9c4d99446
             'actions_user_id': uid,
         }
 >>>>>>> Moved mgmtsystem_nonconformity to root for port
@@ -736,15 +776,28 @@ class mgmtsystem_nonconformity(models.Model):
         self.case_open_send_note(cr, uid, ids, context=context)
         #Open related Actions
 =======
+=======
+            'actions_user_id': self._uid,
+        })
+        self.message_post(
+            body='%s <b>%s</b>' % (
+                self.verbose_name, _('Action Plan Approved')
+            )
+        )
+        return True
+
+    @api.multi
+    def wkf_open(self):
+>>>>>>> Cleanup and full use of v8 and track features
         """Change state from pending approval to in progress, and Open
         the related actions
         """
-        o = self.browse(cr, uid, ids, context=context)[0]
-        if not o.actions_date:
-            raise except_orm(
-                _('Error !'),
+        self.ensure_one()
+        if not self.actions_date:
+            raise exceptions.ValidationError(
                 _('Action plan must be approved before opening.')
             )
+<<<<<<< c2538af8ebe5dc74b251cb209e1c58e9c4d99446
         self.case_open_send_note(cr, uid, ids, context=context)
 <<<<<<< 708ae9983cca04e4dee06cfde6c7c3070f93e28b
         # Open related Actions
@@ -786,9 +839,24 @@ class mgmtsystem_nonconformity(models.Model):
             'evaluation_user_id': None,
         }, context=context)
 >>>>>>> Moved mgmtsystem_nonconformity to root for port
+=======
+        if (self.immediate_action_id and
+                self.immediate_action_id.stage_id.is_starting):
+            self.immediate_action_id.case_open()
+        for action in self.action_ids:
+            if action.stage_id.is_starting:
+                action.case_open()
+        return self.write({
+            'state': 'open',
+            'evaluation_date': False,
+            'evaluation_user_id': False,
+        })
+>>>>>>> Cleanup and full use of v8 and track features
 
-    def action_sign_evaluation(self, cr, uid, ids, context=None):
+    @api.one
+    def action_sign_evaluation(self):
         """Sign-off the effectiveness evaluation"""
+<<<<<<< c2538af8ebe5dc74b251cb209e1c58e9c4d99446
         o = self.browse(cr, uid, ids, context=context)[0]
         if o.state != 'open':
 <<<<<<< c78ad3ce9f0ea5ee98baa923b2662899cec0c8cc
@@ -801,27 +869,40 @@ class mgmtsystem_nonconformity(models.Model):
             raise except_orm(
 >>>>>>> Get exceptions without importing orm
                 _('Error !'),
+=======
+        if self.state != 'open':
+            raise exceptions.ValidationError(
+>>>>>>> Cleanup and full use of v8 and track features
                 _('This action can only be done in the In Progress state.')
             )
-        vals = {
+        self.write({
             'evaluation_date': time.strftime(DATETIME_FORMAT),
+<<<<<<< c2538af8ebe5dc74b251cb209e1c58e9c4d99446
             'evaluation_user_id': uid,
         }
 >>>>>>> Moved mgmtsystem_nonconformity to root for port
         self.write(cr, uid, ids, vals, context=context)
         msg = '%s <b>%s</b>' % (
             self._description, _('Effectiveness Evaluation Approved')
+=======
+            'evaluation_user_id': self._uid,
+        })
+        self.message_post(
+            body='%s <b>%s</b>' % (
+                self.verbose_name, _('Effectiveness Evaluation Approved')
+            )
+>>>>>>> Cleanup and full use of v8 and track features
         )
-        self.message_post(cr, uid, ids, body=msg, context=context)
-        return True
 
-    def wkf_cancel(self, cr, uid, ids, context=None):
+    @api.multi
+    def wkf_cancel(self):
         """Change state to cancel"""
-        self.case_cancel_send_note(cr, uid, ids, context=context)
-        return self.write(cr, uid, ids, {'state': 'cancel'}, context=context)
+        return self.write({'state': 'cancel'})
 
-    def wkf_close(self, cr, uid, ids, context=None):
+    @api.multi
+    def wkf_close(self):
         """Change state from in progress to closed"""
+<<<<<<< c2538af8ebe5dc74b251cb209e1c58e9c4d99446
         o = self.browse(cr, uid, ids, context=context)[0]
 <<<<<<< 13e1999046b4defdeb03ea2574d58c112dd67470
 <<<<<<< 5836488589af7e4b33185380a39618e98d33991e
@@ -849,25 +930,36 @@ class mgmtsystem_nonconformity(models.Model):
                 and not o.immediate_action_id.stage_id.is_ending):
             raise except_orm(
                 _('Error !'),
+=======
+        self.ensure_one()
+
+        if (self.immediate_action_id
+                and not self.immediate_action_id.stage_id.is_ending):
+            raise exceptions.ValidationError(
+>>>>>>> Cleanup and full use of v8 and track features
                 _('Immediate action from analysis has not been closed.')
             )
-        if [i for i in o.action_ids if not i.stage_id.is_ending]:
-            raise except_orm(
-                _('Error !'),
+        if any(i for i in self.action_ids if not i.stage_id.is_ending):
+            raise exceptions.ValidationError(
                 _('Not all actions have been closed.')
             )
-        if not o.evaluation_date:
-            raise except_orm(
-                _('Error !'),
+        if not self.evaluation_date:
+            raise exceptions.ValidationError(
                 _('Effectiveness evaluation must be performed before closing.')
             )
+<<<<<<< c2538af8ebe5dc74b251cb209e1c58e9c4d99446
 >>>>>>> Moved mgmtsystem_nonconformity to root for port
         self.case_close_send_note(cr, uid, ids, context=context)
         return self.write(cr, uid, ids, {'state': 'done'}, context=context)
+=======
+        return self.write({'state': 'done'})
+>>>>>>> Cleanup and full use of v8 and track features
 
-    def case_reset(self, cr, uid, ids, context=None, *args):
-        """Reset to Draft and restart the workflows"""
+    @api.multi
+    def case_reset(self):
+        """Reset to Draft and restart the workflow"""
         wf_service = netsvc.LocalService("workflow")
+<<<<<<< c2538af8ebe5dc74b251cb209e1c58e9c4d99446
         for id in ids:
 <<<<<<< 8a12276cf0affae66506dcba67980c75aac42247
 <<<<<<< b9a32de449f11b9294519ef63ed8a1b78e6eb0f8
@@ -886,10 +978,16 @@ class mgmtsystem_nonconformity(models.Model):
         self.case_reset_send_note(cr, uid, ids, context=context)
 >>>>>>> Moved mgmtsystem_nonconformity to root for port
         vals = {
+=======
+        for nc in self:
+            wf_service.trg_create(self._uid, self._name, nc.id, self._cr)
+        return self.write({
+>>>>>>> Cleanup and full use of v8 and track features
             'state': 'draft',
             'analysis_date': None, 'analysis_user_id': None,
             'actions_date': None, 'actions_user_id': None,
             'evaluation_date': None, 'evaluation_user_id': None,
+<<<<<<< c2538af8ebe5dc74b251cb209e1c58e9c4d99446
         }
         return self.write(cr, uid, ids, vals, context=context)
 <<<<<<< 2394dcc563549fe896d383a41c8d1e4640172f11
@@ -952,3 +1050,6 @@ class mgmtsystem_action(orm.Model):
 >>>>>>> Added missing methodcase_close_send_note for workflows
 =======
 >>>>>>> Remove unused functions
+=======
+        })
+>>>>>>> Cleanup and full use of v8 and track features
