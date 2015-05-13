510b2df1bc97e07bdef2d57512099c6912237363
Sandy Carter <sandy.carter@savoirfairelinux.com>
2015-05-13 11:13:31 -0400

    [IMP+FIX] mgmtsystem_nonconformity: Cleanup + tests + others
    * Move datas to their own folders
    * Cleanup Nonconformity
    * Use track and mail message subtypes instead of overriding functions
    * Remove unused functions
    * Cleanup and full use of v8 and track features
    * Use a python context which does rollback
    * Fix pep8
    * Full test coverage of module

--- mgmtsystem_nonconformity/tests/test_workflows.py
+++ mgmtsystem_nonconformity/tests/test_workflows.py
@@ -19,11 +19,9 @@
 #    along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #
 ##############################################################################
+
+from openerp import exceptions
 from openerp.tests import common
-
-
-class BreakSavePoint(Exception):
-    pass
 
 
 class TestModelNonConformity(common.TransactionCase):
@@ -34,16 +32,6 @@ class TestModelNonConformity(common.TransactionCase):
 
         self.partner_obj = self.partner_m.search([])
 
-    def try_states(self, obj, *methods):
-        """
-        Call multiple methods to check if we can move from
-        one state to an other.
-        """
-        with self.cr.savepoint():
-            for method in methods:
-                with self.assertRaises(Exception):
-                    getattr(obj, method)()
-
     def try_signal(self, obj, signal_name, state, rollback=False):
         """
         Try to trigger a signal.
@@ -51,11 +39,13 @@ class TestModelNonConformity(common.TransactionCase):
         Then rollback to keep working on the same object. As
         if nothing happened.
 
-        The savepoint get rolledback when there is an error.
+        The savepoint get rolled-back when there is an error.
         We use a custom error to catch when there is no error.
         And refresh the model to get back the changes from the
         database.
         """
+        class BreakSavePoint(Exception):
+            pass
         try:
             sid = self.cr.savepoint()
             with sid:
@@ -71,8 +61,8 @@ class TestModelNonConformity(common.TransactionCase):
     def try_cancel(self, obj):
         self.try_signal(obj, 'button_cancel', 'cancel', True)
 
-    def try_invalid_signal(self, obj, signal_name, state):
-        with self.assertRaises(Exception):
+    def try_invalid_signal(self, obj, signal_name, state, msg=None):
+        with self.assertRaises(Exception, msg=msg):
             self.try_signal(obj, signal_name, state, True)
 
     def test_workflow(self):
@@ -110,7 +100,6 @@ class TestModelNonConformity(common.TransactionCase):
         # open -> done
         self.try_invalid_signal(conf_obj, 'button_close', 'done')
         # draft-> analysis
-        # self.try_invalid_signal(conf_obj, 'button_analysis_n', 'analysis')
         self.try_signal(conf_obj, 'button_analysis_n', 'analysis')
         self.assertEqual(conf_obj._state_name()[conf_obj.id], 'Analysis')
         self.try_cancel(conf_obj)
@@ -120,8 +109,6 @@ class TestModelNonConformity(common.TransactionCase):
         self.try_signal(conf_obj, 'button_analysis_p', 'analysis')
         # draft-> analysis (already on analysis)
         self.try_signal(conf_obj, 'button_analysis_n', 'analysis')
-        # analysis -> pending
-        # self.try_invalid_signal(conf_obj, 'button_review_n', 'pending')
         # open -> pending
         self.try_invalid_signal(conf_obj, 'button_review_p', 'pending')
         # pending-> open
@@ -154,32 +141,50 @@ class TestModelNonConformity(common.TransactionCase):
         self.try_cancel(conf_obj)
 
         # Test Pending to Open
-        # pending -> analysis
-        # self.try_invalid_signal(conf_obj, 'button_analysis_p', 'analysis')
         # draft-> analysis
         self.try_invalid_signal(conf_obj, 'button_analysis_n', 'analysis')
         # analysis -> pending
         self.try_signal(conf_obj, 'button_review_n', 'pending')
         # open -> pending
         self.try_signal(conf_obj, 'button_review_p', 'pending')
-        # pending-> open
-        # self.try_invalid_signal(conf_obj, 'button_open', 'open')
         # open -> done
         self.try_invalid_signal(conf_obj, 'button_close', 'done')
 
         self.try_invalid_signal(conf_obj, 'button_open', 'open')
 
-        with self.cr.savepoint():
-            with self.assertRaises(Exception):
-                conf_obj.action_sign_analysis()
-        with self.cr.savepoint():
-            with self.assertRaises(Exception):
-                conf_obj.action_sign_evaluation()
+        with self.assertRaises(exceptions.ValidationError), \
+                self.cr.savepoint():
+            conf_obj.action_sign_analysis()
+        with self.assertRaises(exceptions.ValidationError), \
+                self.cr.savepoint():
+            conf_obj.action_sign_evaluation()
+
+        with self.assertRaises(
+                exceptions.ValidationError,
+                msg="System should have prevented signing before the "
+                    "review confirmation"
+        ), self.cr.savepoint():
+            conf_obj.write({"analysis_date": False})
+            conf_obj.action_sign_actions()
 
         conf_obj.action_sign_actions()
-        with self.cr.savepoint():
-            with self.assertRaises(Exception):
-                conf_obj.action_sign_actions()
+        with self.assertRaises(
+                exceptions.ValidationError,
+                msg="System should have double signing"
+        ), self.cr.savepoint():
+            conf_obj.action_sign_actions()
+        immediate_action = self.env['mgmtsystem.action'].create({
+            "name": "Test Immediate Action",
+            "reference": "Test Immediate Action",
+        })
+        action = self.env['mgmtsystem.action'].create({
+            "name": "Test Action",
+            "reference": "Test Action",
+        })
+        conf_obj.write({
+            "immediate_action_id": immediate_action.id,
+            "action_ids": [(6, 0, [action.id])],
+        })
         self.try_signal(conf_obj, 'button_open', 'open')
         self.assertEqual(conf_obj._state_name()[conf_obj.id], 'In Progress')
 
@@ -195,15 +200,38 @@ class TestModelNonConformity(common.TransactionCase):
         # open -> pending
         self.try_signal(conf_obj, 'button_review_p', 'pending', True)
         # open -> done
-        self.try_invalid_signal(conf_obj, 'button_close', 'done')
+        self.try_invalid_signal(
+            conf_obj, 'button_close', 'done',
+            msg="System should have blocked closing a Nonconformity with "
+                "non-closed immediate actions"
+        )
+        immediate_action.write({
+            "stage_id": self.env['mgmtsystem.action.stage'].search([
+                ('name', '=', "Closed"),
+            ]).id
+        })
+        self.try_invalid_signal(
+            conf_obj, 'button_close', 'done',
+            msg="System should have blocked closing a Nonconformity with "
+                "non-closed actions"
+        )
+        action.write({
+            "stage_id": self.env['mgmtsystem.action.stage'].search([
+                ('name', '=', "Closed"),
+            ]).id
+        })
+        self.try_invalid_signal(
+            conf_obj, 'button_close', 'done',
+            msg="System should have blocked closing a Nonconformity without "
+                "effectiveness evaluation being performed."
+        )
 
-        with self.cr.savepoint():
-            with self.assertRaises(Exception):
-                conf_obj.action_sign_analysis()
-        with self.cr.savepoint():
-            with self.assertRaises(Exception):
-                conf_obj.action_sign_actions()
-
+        with self.assertRaises(exceptions.ValidationError), \
+                self.cr.savepoint():
+            conf_obj.action_sign_analysis()
+        with self.assertRaises(exceptions.ValidationError), \
+                self.cr.savepoint():
+            conf_obj.action_sign_actions()
         conf_obj.action_sign_evaluation()
         self.try_signal(conf_obj, 'button_close', 'done')
         self.assertEqual(conf_obj._state_name()[conf_obj.id], 'Closed')
