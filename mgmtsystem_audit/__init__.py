128f55034c73d6d84c88c09e180479da8e8f4725
Maxime Chambreuil <maxime.chambreuil@savoirfairelinux.com>
2015-08-05 14:54:18 -0400

    [IMP] Module structure

--- mgmtsystem_audit/__init__.py
+++ mgmtsystem_audit/__init__.py
@@ -1,3 +1,4 @@
+<<<<<<< 0bb2f475778b7c2c2694bf72154fdacddbde7d91
 # -*- encoding: utf-8 -*-
 <<<<<<< a388c9b0a4cc1dd786ea7063756aefdf4864657a
 <<<<<<< 697b7c1967849d398f6212cef8d15618f8ce3201
@@ -18,3 +19,11 @@ from . import wizard
 # vim:expandtab:smartindent:tabstop=4:softtabstop=4:shiftwidth=4:
 =======
 >>>>>>> Removed vim lines
+=======
+# -*- coding: utf-8 -*-
+from . import (
+    models,
+    report,
+    wizard
+)
+>>>>>>> [IMP] Module structure
--- mgmtsystem_audit/report/__init__.py
+++ mgmtsystem_audit/report/__init__.py
@@ -1,3 +1,4 @@
+<<<<<<< 0bb2f475778b7c2c2694bf72154fdacddbde7d91
 # -*- encoding: utf-8 -*-
 <<<<<<< a388c9b0a4cc1dd786ea7063756aefdf4864657a
 <<<<<<< 697b7c1967849d398f6212cef8d15618f8ce3201
@@ -20,3 +21,10 @@ from . import verification_list
 # vim:expandtab:smartindent:tabstop=4:softtabstop=4:shiftwidth=4:
 =======
 >>>>>>> Removed vim lines
+=======
+# -*- coding: utf-8 -*-
+from . import (
+    audit_report,
+    verification_list
+)
+>>>>>>> [IMP] Module structure
