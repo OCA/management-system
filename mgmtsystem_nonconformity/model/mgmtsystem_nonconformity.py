9ae530a94541730bcdce234e2bcad8e8c82804ef
Maxime Chambreuil <maxime.chambreuil@savoirfairelinux.com>
2015-08-20 16:08:29 -0400

    [FIX] #86

--- mgmtsystem_nonconformity/model/mgmtsystem_nonconformity.py
+++ mgmtsystem_nonconformity/model/mgmtsystem_nonconformity.py
@@ -201,10 +201,8 @@ class MgmtsystemNonconformity(base_state, orm.Model):
             msg = '%s <b>%s</b>' % (pre, text)
             if data:
                 o = self.browse(cr, uid, ids, context=context)[0]
-                post = u'''
-<br />
-<ul><li> <b>''' + _('Stage:') + '''</b> %s →  %s</li></ul>''' % (o.state,
-                                                                 data['state'])
+                post = '''<br /><ul><li> <b>''' + _('Stage:') + u'''</b> \
+%s → %s</li></ul>''' % (o.state, data['state'])
                 msg += post
             self.message_post(cr, uid, [id], body=msg, context=context)
         return True
