a786d3a2db4d1d117d0b7e802c531366a4bd1299
Stéphane Bidoul <stephane.bidoul@acsone.eu>
2015-10-09 10:01:45 +0200

    [UPD] prefix versions with 8.0

--- document_page_health_safety_manual/__openerp__.py
+++ document_page_health_safety_manual/__openerp__.py
@@ -20,8 +20,8 @@
 ##############################################################################
 {
     "name": "Document Management - Wiki - Health and Safety Manual",
-    "version": "1.0",
-    "author": "Savoir-faire Linux",
+    "version": "8.0.1.0.0",
+    "author": "Savoir-faire Linux,Odoo Community Association (OCA)",
     "website": "http://www.savoirfairelinux.com",
     "license": "AGPL-3",
     "category": "Generic Modules/Others",
