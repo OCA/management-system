dc82b348b28327f4aa8a498cc82cd84f05b51da1
Stéphane Bidoul <stephane.bidoul@acsone.eu>
2015-10-09 10:01:45 +0200

    [UPD] prefix versions with 8.0

--- document_page_environmental_aspect/__openerp__.py
+++ document_page_environmental_aspect/__openerp__.py
@@ -20,7 +20,7 @@
 ##############################################################################
 {
     "name": "Document Management - Wiki - Environmental Aspects",
-    "version": "1.0",
+    "version": "8.0.1.0.0",
     "author": "Savoir-faire Linux,Odoo Community Association (OCA)",
     "website": "http://www.savoirfairelinux.com",
     "license": "AGPL-3",
