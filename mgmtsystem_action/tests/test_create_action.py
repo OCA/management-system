331cee25bf05b99b51c6bb3f11e4749736f00f6b
Gervais Naoussi <gervaisnaoussi@gmail.com>
2016-06-09 13:37:23 -0400

    add the case_open method, because it is usefull for mgmtsystem_nonconformity

--- mgmtsystem_action/tests/test_create_action.py
+++ mgmtsystem_action/tests/test_create_action.py
@@ -15,6 +15,27 @@ class TestModelAction(common.TransactionCase):
         self.assertEqual(record.stage_id.is_starting, True)
 
     def test_case_open(self):
+<<<<<<< 30b7755499fb1bc52d6afb3c3e19c803eb3ba928
+=======
+        record = self.env['mgmtsystem.action'].create({
+            "name": "SampleAction",
+            "type_action": "immediate",
+        })
+
+        record.active = False
+
+        ret = record.case_open()
+
+        self.assertEqual(ret, True)
+        self.assertEqual(record.active, True)
+        self.assertEqual(record.stage_id.name, 'In Progress')
+        self.assertEqual(record.stage_id.is_starting, False)
+        self.assertEqual(record.stage_id.is_ending, False)
+
+    def test_case_close(self):
+        """Test object close state."""
+        stage = self.env.ref('mgmtsystem_action.stage_close')
+>>>>>>> add the case_open method, because it is usefull for mgmtsystem_nonconformity
         record = self.env['mgmtsystem.action'].create({
             "name": "SampleAction",
             "type_action": "immediate",
