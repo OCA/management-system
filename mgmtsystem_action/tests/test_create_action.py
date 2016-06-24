7924ec471ee68dab23b5e2a513c597b813e73e1c
Gervais Naoussi <gervaisnaoussi@gmail.com>
2016-06-23 21:24:21 -0400

    Chatter notify when the actions progress through stages
    Fixing test error
    
    Fixing error by making test work
    
    Improving test coverage
    
    Fixing pylint error
    
    Remove cancel date when moving from cancel stage to draft or open stage
    
    Fixing stage_id error
    
    OCA Transbot updated translations from Transifex

--- mgmtsystem_action/tests/test_create_action.py
+++ mgmtsystem_action/tests/test_create_action.py
@@ -1,3 +1,9 @@
+<<<<<<< a235f9511a527a31b4f2a7a2a68f45c3463732e0
+=======
+# -*- coding: utf-8 -*-
+
+from openerp import exceptions
+>>>>>>> Improving test coverage
 from openerp.tests import common
 
 
@@ -5,7 +11,11 @@ class TestModelAction(common.TransactionCase):
     def test_create_action(self):
         record = self.env['mgmtsystem.action'].create({
             "name": "SampleAction",
+<<<<<<< 1b8e49b877d1f53e685156cb680857f6d57d1f8c
             "type_action": "immediate",
+=======
+            "type_action": "immediate"
+>>>>>>> Fixing test error
         })
 
         self.assertEqual(record.name, "SampleAction")
@@ -15,14 +25,19 @@ class TestModelAction(common.TransactionCase):
         self.assertEqual(record.stage_id.is_starting, True)
 
     def test_case_open(self):
+<<<<<<< f6b774890daa11142d5dd0f33efc18252f456ac9
 <<<<<<< 30b7755499fb1bc52d6afb3c3e19c803eb3ba928
 =======
+=======
+        """Test object open state."""
+>>>>>>> Fixing error by making test work
         record = self.env['mgmtsystem.action'].create({
             "name": "SampleAction",
             "type_action": "immediate",
         })
 
-        record.active = False
+        record.write(
+            {'active': False, 'stage_id': record._get_stage_open().id})
 
         ret = record.case_open()
 
@@ -31,15 +46,29 @@ class TestModelAction(common.TransactionCase):
         self.assertEqual(record.stage_id.name, 'In Progress')
         self.assertEqual(record.stage_id.is_starting, False)
         self.assertEqual(record.stage_id.is_ending, False)
+
+    def test_get_new_stage(self):
+        """Get stage new."""
+        record = self.env['mgmtsystem.action'].create({
+            "name": "SampleAction",
+            "type_action": "immediate",
+        })
+        stage = record._get_stage_new()
+
+        self.assertEqual(stage.name, 'Draft')
 
     def test_case_close(self):
         """Test object close state."""
+<<<<<<< a235f9511a527a31b4f2a7a2a68f45c3463732e0
         stage = self.env.ref('mgmtsystem_action.stage_close')
 >>>>>>> add the case_open method, because it is usefull for mgmtsystem_nonconformity
+=======
+>>>>>>> Improving test coverage
         record = self.env['mgmtsystem.action'].create({
             "name": "SampleAction",
             "type_action": "immediate",
         })
+<<<<<<< a235f9511a527a31b4f2a7a2a68f45c3463732e0
 
         record.active = False
 
@@ -50,6 +79,35 @@ class TestModelAction(common.TransactionCase):
         self.assertEqual(record.stage_id.name, 'In Progress')
         self.assertEqual(record.stage_id.is_starting, False)
         self.assertEqual(record.stage_id.is_ending, False)
+=======
+        stage = record._get_stage_open()
+        stage_new = record._get_stage_new()
+        record.stage_id = stage
+        stage = record._get_stage_close()
+        record.stage_id = stage
+        self.assertEqual(record.date_closed[:-3], datetime.now().strftime(
+            '%Y-%m-%d %H:%M'
+        ))
+        try:
+            record.write({'stage_id': stage_new.id})
+        except exceptions.ValidationError:
+            self.assertTrue(True)
+        stage = record._get_stage_cancel()
+        record.stage_id = stage
+        self.assertFalse(record.date_closed)
+        self.assertFalse(record.opening_date)
+        stage = record._get_stage_close()
+        try:
+            record.write({'stage_id': stage.id})
+        except exceptions.ValidationError:
+            self.assertTrue(True)
+<<<<<<< 30ebbfd9670688c1eb756e7fe5a91f0b68aa67b7
+>>>>>>> Improving test coverage
+=======
+        record.write({'stage_id': stage_new.id})
+        self.assertFalse(record.date_closed)
+        self.assertFalse(record.opening_date)
+>>>>>>> Fixing pylint error
 
     def test_get_action_url(self):
         record = self.env['mgmtsystem.action'].create({
