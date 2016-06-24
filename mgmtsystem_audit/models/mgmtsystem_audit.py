56b8076638e36d28303d4616169c4a77e21a31c6
Gervais Naoussi <gervaisnaoussi@gmail.com>
2016-06-23 20:13:04 -0400

    [MIG] mgmtsystem_audit

--- mgmtsystem_audit/models/mgmtsystem_audit.py
+++ mgmtsystem_audit/models/mgmtsystem_audit.py
@@ -1,4 +1,4 @@
-# -*- encoding: utf-8 -*-
+# -*- coding: utf-8 -*-
 ##############################################################################
 #
 #    OpenERP, Open Source Management Solution
@@ -19,164 +19,199 @@
 #
 ##############################################################################
 
-from openerp.tools.translate import _
-from openerp.osv import fields, orm
-from urllib import urlencode
-from urlparse import urljoin
+from openerp import fields, models, api, _
 
 
-class mgmtsystem_audit(orm.Model):
+class MgmtSystemAudit(models.Model):
+    """Model class that manage audit."""
+
     _name = "mgmtsystem.audit"
     _description = "Audit"
     _inherit = ['mail.thread']
-    _columns = {
-        'name': fields.char('Name', size=50),
-        'reference': fields.char(
-            'Reference',
-            size=64,
-            required=True,
-            readonly=True,
-        ),
-        'date': fields.datetime('Date'),
-        'line_ids': fields.one2many(
-            'mgmtsystem.verification.line',
-            'audit_id',
-            'Verification List',
-        ),
-        'user_id': fields.many2one('res.users', 'Audit Manager'),
-        'auditor_user_ids': fields.many2many(
-            'res.users',
-            'mgmtsystem_auditor_user_rel',
-            'user_id',
-            'mgmtsystem_audit_id',
-            'Auditors',
-        ),
-        'auditee_user_ids': fields.many2many(
-            'res.users',
-            'mgmtsystem_auditee_user_rel',
-            'user_id',
-            'mgmtsystem_audit_id',
-            'Auditees',
-        ),
-        'strong_points': fields.text('Strong Points'),
-        'to_improve_points': fields.text('Points To Improve'),
-        'imp_opp_ids': fields.many2many(
-            'mgmtsystem.action',
-            'mgmtsystem_audit_imp_opp_rel',
-            'mgmtsystem_action_id',
-            'mgmtsystem_audit_id',
-            'Improvement Opportunities',
-        ),
-        'nonconformity_ids': fields.many2many(
-            'mgmtsystem.nonconformity',
-            string='Nonconformities',
-        ),
-        'state': fields.selection(
-            [
-                ('open', 'Open'),
-                ('done', 'Closed'),
-            ],
-            'State',
-        ),
-        'system_id': fields.many2one('mgmtsystem.system', 'System'),
-        'company_id': fields.many2one('res.company', 'Company')
-    }
+    name = fields.Char('Name')
 
-    _defaults = {
-        'company_id': (
-            lambda self, cr, uid, c:
-            self.pool.get('res.users').browse(cr, uid, uid, c).company_id.id),
-        'reference': 'NEW',
-        'state': 'open'
-    }
+    reference = fields.Char(
+        'Reference',
+        size=64,
+        required=True,
+        readonly=True,
+        default='NEW'
+    )
+    date = fields.Datetime('Date')
+    line_ids = fields.One2many(
+        'mgmtsystem.verification.line',
+        'audit_id',
+        'Verification List',
+    )
+    number_of_audits = fields.Integer('# of audits', readonly=True, default=1)
+    number_of_nonconformities = fields.Integer(
+        'Number of nonconformities', readonly=True, store=True,
+        compute="_compute_number_of_nonconformities")
+    number_of_questions_in_verification_list = fields.Integer(
+        'Number of questions in verification list', readonly=True, store=True,
+        compute="_compute_number_of_questions_in_verification_list")
+    number_of_improvements_opportunity = fields.Integer(
+        'Number of improvements Opportunities', readonly=True, store=True,
+        compute="_compute_number_of_improvement_opportunities")
+    days_since_last_update = fields.Integer(
+        'Days since last update', readonly=True, store=True,
+        compute="_compute_days_since_last_update")
+    closing_date = fields.Datetime('Closing Date', readonly=True)
 
-    def create(self, cr, uid, vals, context=None):
+    number_of_days_to_close = fields.Integer(
+        '# of days to close', readonly=True, store=True,
+        compute="_compute_number_of_days_to_close")
+
+    user_id = fields.Many2one('res.users', 'Audit Manager')
+    auditor_user_ids = fields.Many2many(
+        'res.users',
+        'mgmtsystem_auditor_user_rel',
+        'user_id',
+        'mgmtsystem_audit_id',
+        'Auditors',
+    )
+    auditee_user_ids = fields.Many2many(
+        'res.users',
+        'mgmtsystem_auditee_user_rel',
+        'user_id',
+        'mgmtsystem_audit_id',
+        'Auditees',
+    )
+    strong_points = fields.Html('Strong Points')
+    to_improve_points = fields.Html('Points To Improve')
+    imp_opp_ids = fields.Many2many(
+        'mgmtsystem.action',
+        'mgmtsystem_audit_imp_opp_rel',
+        'mgmtsystem_action_id',
+        'mgmtsystem_audit_id',
+        'Improvement Opportunities',
+    )
+
+    nonconformity_ids = fields.Many2many(
+        'mgmtsystem.nonconformity',
+        string='Nonconformities',
+    )
+    state = fields.Selection(
+        [
+            ('open', 'Open'),
+            ('done', 'Closed'),
+        ],
+        'State',
+        default="open"
+    )
+    system_id = fields.Many2one('mgmtsystem.system', 'System')
+    company_id = fields.Many2one(
+        'res.company', 'Company',
+        default=lambda self: self.env.user.company_id.id)
+
+    @api.depends("nonconformity_ids")
+    def _compute_number_of_nonconformities(self):
+        """Count number of nonconformities."""
+        for audit in self:
+            audit.number_of_nonconformities = len(self.nonconformity_ids)
+
+    @api.depends("imp_opp_ids")
+    def _compute_number_of_improvement_opportunities(self):
+        """Count number of improvements Opportunities."""
+        for audit in self:
+            audit.number_of_improvements_opportunity = len(audit.imp_opp_ids)
+
+    @api.depends("line_ids")
+    def _compute_number_of_questions_in_verification_list(self):
+        for audit in self:
+            audit.number_of_questions_in_verification_list = len(
+                audit.line_ids)
+
+    @api.depends("write_date")
+    def _compute_days_since_last_update(self):
+        for audit in self:
+            audit.days_since_last_update = audit._elapsed_days(
+                audit.create_date,
+                audit.write_date)
+
+    @api.depends("closing_date")
+    def _compute_number_of_days_to_close(self):
+        for audit in self:
+            audit.number_of_days_to_close = audit._elapsed_days(
+                audit.create_date,
+                audit.closing_date)
+
+    @api.model
+    def _elapsed_days(self, dt1_text, dt2_text):
+        res = 0
+        if dt1_text and dt2_text:
+            dt1 = fields.Datetime.from_string(dt1_text)
+            dt2 = fields.Datetime.from_string(dt2_text)
+            res = (dt2 - dt1).days
+        return res
+
+    @api.model
+    def create(self, vals):
+        """Audit creation."""
         vals.update({
-            'reference': self.pool.get('ir.sequence').get(
-                cr, uid, 'mgmtsystem.audit'
-            )
+            'reference': self.env['ir.sequence'].next_by_code(
+                'mgmtsystem.audit'
+            ),
         })
-        return super(mgmtsystem_audit, self).create(
-            cr, uid, vals, context=context)
+        audit_id = super(MgmtSystemAudit, self).create(vals)
+        return audit_id
 
-    def button_close(self, cr, uid, ids, context=None):
+    @api.multi
+    def button_close(self):
         """When Audit is closed, post a message to followers' chatter."""
-        self.message_post(cr, uid, ids, _("Audit closed"), context=context)
-        return self.write(cr, uid, ids, {'state': 'done'})
+        self.message_post(_("Audit closed"))
+        return self.write({'state': 'done',
+                           'closing_date': fields.Datetime.now()})
 
-    def message_auto_subscribe(
-            self, cr, uid, ids, updated_fields, context=None, values=None):
-        """Automatically add the Auditors, Auditees and Audit Manager
-        to the follow list
-        """
-        for o in self.browse(cr, uid, ids, context=context):
-            user_ids = [o.user_id.id]
-            user_ids += [a.id for a in o.auditor_user_ids]
-            user_ids += [a.id for a in o.auditee_user_ids]
-            self.message_subscribe_users(
-                cr, uid, ids, user_ids=user_ids, subtype_ids=None,
-                context=context
-            )
-        return super(mgmtsystem_audit, self).message_auto_subscribe(
-            cr, uid, ids, updated_fields, context=context, values=values
-        )
-
-    def get_audit_url(self, cr, uid, ids, context=None):
+    def get_action_url(self):
         """
         Return a short link to the audit form view
         eg. http://localhost:8069/?db=prod#id=1&model=mgmtsystem.audit
         """
-        assert len(ids) == 1
-        audit = self.browse(cr, uid, ids[0], context=context)
-        base_url = self.pool.get('ir.config_parameter').get_param(
-            cr, uid, 'web.base.url', default='http://localhost:8069',
-            context=context,
+
+        base_url = self.env['ir.config_parameter'].get_param(
+            'web.base.url',
+            default='http://localhost:8069'
         )
-        query = {'db': cr.dbname}
-        fragment = {'id': audit.id, 'model': self._name}
-        return urljoin(
-            base_url, "?%s#%s" %
-            (urlencode(query), urlencode(fragment))
+        url = ('{}/web#db={}&id={}&model={}').format(
+            base_url,
+            self.env.cr.dbname,
+            self.id,
+            self._name
         )
+        return url
 
+    def get_lines_by_procedure(self):
+        p = []
+        for l in self.line_ids:
+            if l.procedure_id.id:
+                proc_nm = self.pool.get('document.page').read(
+                    self.env.cr, self.env.uid, l.procedure_id.id, ['name']
+                )
+                procedure_name = proc_nm['name']
+            else:
+                procedure_name = _('Undefined')
 
-class mgmtsystem_verification_line(orm.Model):
-    _name = "mgmtsystem.verification.line"
-    _description = "Verification Line"
-    _columns = {
-        'name': fields.char('Question', size=300, required=True),
-        'audit_id': fields.many2one(
-            'mgmtsystem.audit',
-            'Audit',
-            ondelete='cascade',
-            select=True,
-        ),
-        'procedure_id': fields.many2one(
-            'document.page',
-            'Procedure',
-            ondelete='cascade',
-            select=True,
-        ),
-        'is_conformed': fields.boolean('Is conformed'),
-        'comments': fields.text('Comments'),
-        'seq': fields.integer('Sequence'),
-        'company_id': fields.many2one('res.company', 'Company')
-    }
-
-    _order = "seq"
-    _defaults = {
-        'company_id': (
-            lambda self, cr, uid, c:
-            self.pool.get('res.users').browse(cr, uid, uid, c).company_id.id
-        ),
-        'is_conformed': False
-    }
-
-
-class mgmtsystem_nonconformity(orm.Model):
-    _name = "mgmtsystem.nonconformity"
-    _inherit = "mgmtsystem.nonconformity"
-    _columns = {
-        'audit_ids': fields.many2many(
-            'mgmtsystem.audit', string='Related Audits'),
-    }
+            p.append({"id": l.id,
+                      "procedure": procedure_name,
+                      "name": l.name,
+                      "yes_no": "Yes / No"})
+        p = sorted(p, key=lambda k: k["procedure"])
+        proc_line = False
+        q = []
+        proc_name = ''
+        for i in range(len(p)):
+            if proc_name != p[i]['procedure']:
+                proc_line = True
+            if proc_line:
+                q.append({"id": p[i]['id'],
+                          "procedure": p[i]['procedure'],
+                          "name": "",
+                          "yes_no": ""})
+                proc_line = False
+                proc_name = p[i]['procedure']
+            q.append({"id": p[i]['id'],
+                      "procedure": "",
+                      "name": p[i]['name'],
+                      "yes_no": "Yes / No"})
+        return q
