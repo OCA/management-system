b6241ddcf65024d0c82b8317ab90d8159e50304c
Gervais Naoussi <gervaisnaoussi@gmail.com>
2016-08-11 08:26:46 -0400

    adding improvements

--- mgmtsystem_kpi/__openerp__.py
+++ mgmtsystem_kpi/__openerp__.py
@@ -20,7 +20,7 @@
 ##############################################################################
 {
     "name": "Key Performance Indicator",
-    "version": "1.1",
+    "version": "9.0.1.0.0",
     "author": "Savoir-faire Linux,Odoo Community Association (OCA)",
     "website": "http://www.savoirfairelinux.com",
     "license": "AGPL-3",
