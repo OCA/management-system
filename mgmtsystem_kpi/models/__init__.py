c68179eefe3e51463051f2c090c2b6a47adece82
Gervais Naoussi <gervaisnaoussi@gmail.com>
2016-09-06 13:00:23 -0400

    mgmtsystem_kpi is just a reference to kpi now

--- mgmtsystem_kpi/models/__init__.py
+++
@@ -1,7 +0,0 @@
-# -*- coding: utf-8 -*-
-
-from . import mgmtsystem_kpi_category
-from . import mgmtsystem_kpi_threshold_range
-from . import mgmtsystem_kpi_threshold
-from . import mgmtsystem_kpi_history
-from . import mgmtsystem_kpi
