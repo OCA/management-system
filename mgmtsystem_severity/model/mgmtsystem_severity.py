e4bd7e15dadc109157b67d66a9741f9cf77eb01e
Gervais Naoussi <gervaisnaoussi@gmail.com>
2016-09-23 11:16:50 -0400

    mgmtsystem_severity migration 9.0

--- mgmtsystem_severity/model/mgmtsystem_severity.py
+++ mgmtsystem_severity/model/mgmtsystem_severity.py
@@ -1,29 +1,9 @@
 # -*- coding: utf-8 -*-
-##############################################################################
-#
-#    OpenERP, Open Source Management Solution
-#    Copyright (C) 2015 - Present
-#    Savoir-faire Linux (<http://www.savoirfairelinux.com>).
-#
-#    This program is free software: you can redistribute it and/or modify
-#    it under the terms of the GNU Affero General Public License as
-#    published by the Free Software Foundation, either version 3 of the
-#    License, or (at your option) any later version.
-#
-#    This program is distributed in the hope that it will be useful,
-#    but WITHOUT ANY WARRANTY; without even the implied warranty of
-#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-#    GNU Affero General Public License for more details.
-#
-#    You should have received a copy of the GNU Affero General Public License
-#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
-#
-##############################################################################
-from openerp.tools.translate import _
-from osv import fields, orm
+
+from openerp import fields, models, _
 
 
-class MgmtSystemSeverity(orm.Model):
+class MgmtSystemSeverity(models.Model):
 
     """
     Define the Severity for management system.
@@ -35,35 +15,13 @@ class MgmtSystemSeverity(orm.Model):
     _name = "mgmtsystem.severity"
     _description = "Management System Severity"
 
-    def __category_selection(self, cr, uid, context=None):
-        """
-        Return the category selection.
+    name = fields.Char("Name", required=True, translate=True)
 
-        This method act as a proxy between the model method
-        _category_selection and odoo. Subclassing the method
-        used in the field.selection has no effect. Subclass
-        the _category_selection method instead.
-        """
-        return self._category_selection(cr, uid, context)
+    description = fields.Text("Description")
 
-    _columns = {
-        "name": fields.char("Name", required=True, translate=True),
-        "description": fields.text("Description"),
-        "value": fields.integer("Value", required=True),
-        "category": fields.selection(
-            __category_selection,
-            "Category",
-            required=True
-        ),
-    }
+    value = fields.Integer("Value", required=True)
 
-    def _category_selection(self, cr, uid, context=None):
-        """
-        Return the category selection.
-
-        This method can be subclassed by other classes.
-        """
-        return [
-            ("hazard", _("hazard")),
-            ("security", _("security")),
-        ]
+    category = fields.Selection((
+        ("hazard", _("hazard")),
+        ("security", _("security")),
+    ), 'Category', required=True)
