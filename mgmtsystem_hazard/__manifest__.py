0e0b6f118d19ce0a9d046251b7205e268ff55779
Nadege Grandjean <nadege.grandjean@logicasoft.eu>
2017-02-03 15:17:26 +0100

    change indicated version to 9.0

--- mgmtsystem_hazard/__manifest__.py
+++ mgmtsystem_hazard/__manifest__.py
@@ -20,7 +20,7 @@
 ##############################################################################
 {
     "name": "Hazard",
-    "version": "8.0.1.1.0",
+    "version": "9.0.1.1.0",
     "author": "Savoir-faire Linux, Odoo Community Association (OCA)",
     "website": "http://www.savoirfairelinux.com",
     "license": "AGPL-3",
