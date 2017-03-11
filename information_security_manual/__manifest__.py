779bb45e5e0d8f8519b32bd7494f916112c997cc
Eugen Don <eugendon87@gmail.com>
2017-03-11 21:06:54 +0100

    MIG to V. 10.0
    MIG to V. 10.0

---
+++ information_security_manual/__manifest__.py
@@ -0,0 +1,39 @@
+# -*- coding: utf-8 -*-
+##############################################################################
+#
+#    OpenERP, Open Source Management Solution
+#    Copyright (C) 2010 Savoir-faire Linux (<http://www.savoirfairelinux.com>).
+#
+#    This program is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU Affero General Public License as
+#    published by the Free Software Foundation, either version 3 of the
+#    License, or (at your option) any later version.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU Affero General Public License for more details.
+#
+#    You should have received a copy of the GNU Affero General Public License
+#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#
+##############################################################################
+{
+    "name": "Information Security Management System Manual",
+    "version": "8.0.1.0.0",
+    "author": "Savoir-faire Linux,Odoo Community Association (OCA)",
+    "website": "http://www.savoirfairelinux.com",
+    "license": "AGPL-3",
+    "category": "Generic Modules/Others",
+    "description": """Information Security Manual Template
+    """,
+    "depends": [
+        'mgmtsystem_manuals'
+    ],
+    "init": [],
+    "data": [
+        'data/document_page.xml',
+    ],
+    "demo": [],
+    'installable': True,
+}
