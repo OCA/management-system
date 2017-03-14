6096f3fa62a8950dbf4582bb2c6f1137c27d4100
Eugen Don <eugendon87@gmail.com>
2017-03-14 12:04:44 +0100

    MIG-mgmtsystem_survey-to-V.-10.0
    MIG-mgmtsystem_survey-to-V.-10.0

--- mgmtsystem_survey/__manifest__.py
+++ mgmtsystem_survey/__manifest__.py
@@ -1,23 +1,7 @@
 # -*- coding: utf-8 -*-
-##############################################################################
-#
-#    OpenERP, Open Source Management Solution
-#    Copyright (C) 2010 Savoir-faire Linux (<http://www.savoirfairelinux.com>).
-#
-#    This program is free software: you can redistribute it and/or modify
-#    it under the terms of the GNU Affero General Public License as
-#    published by the Free Software Foundation, either version 3 of the
-#    License, or (at your option) any later version.
-#
-#    This program is distributed in the hope that it will be useful,
-#    but WITHOUT ANY WARRANTY; without even the implied warranty of
-#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-#    GNU Affero General Public License for more details.
-#
-#    You should have received a copy of the GNU Affero General Public License
-#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
-#
-##############################################################################
+# Copyright (C) 2010 Savoir-faire Linux (<http://www.savoirfairelinux.com>).
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+
 {
 <<<<<<< 3ef0d6681c9f2b5d1e2d5250a3a97b2ed9d9b6eb
 <<<<<<< 1079161f42ff13d72cc09e135702bf9974ec0d26
@@ -53,7 +37,7 @@
 # vim:expandtab:smartindent:tabstop=4:softtabstop=4:shiftwidth=4:
 =======
     "name": "Management System - Survey",
-    "version": "9.0.1.0.0",
+    "version": "10.0.1.0.0",
     "author": "Savoir-faire Linux,Odoo Community Association (OCA)",
     "website": "http://www.savoirfairelinux.com",
     "license": "AGPL-3",
@@ -68,6 +52,6 @@
     ],
     "demo": [
     ],
-    'installable': False,
+    'installable': True,
 }
 >>>>>>> Added mgmtsystem_survey to ported code
