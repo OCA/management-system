f6a2290fda0c7a8a3af9cc9cf6e1f3c8fa113fab
Eugen Don <eugendon87@gmail.com>
2017-03-14 11:53:37 +0100

    MIG-mgmtsystem_nonconformity-to-V.-10.0
    MIG-mgmtsystem_nonconformity-to-V.-10.0

--- mgmtsystem_nonconformity/models/mgmtsystem_nonconformity_origin.py
+++ mgmtsystem_nonconformity/models/mgmtsystem_nonconformity_origin.py
@@ -2,7 +2,7 @@
 # Copyright (C) 2010 Savoir-faire Linux (<http://www.savoirfairelinux.com>).
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 
-from openerp import fields, models, api
+from odoo import fields, models, api
 
 
 class MgmtsystemNonconformityOrigin(models.Model):
