74271c200758bfa5c7be303864603a95d236d2c3
Eugen Don <eugendon87@gmail.com>
2017-06-13 09:23:28 +0200

    MIG mgmtsystem_review to V. 10.0

--- mgmtsystem_review/__manifest__.py
+++ mgmtsystem_review/__manifest__.py
@@ -1,23 +1,7 @@
 # -*- coding: utf-8 -*-
-##############################################################################
-#
-#    OpenERP, Open Source Management Solution
-#    Copyright (C) 2010 Savoir-faire Linux (<http://www.savoirfairelinux.com>).
-#
-#    This program is free software: you can redistribute it and/or modify
-#    it under the terms of the GNU Affero General Public License as
-#    published by the Free Software Foundation, either version 3 of the
-#    License, or (at your option) any later version.
-#
-#    This program is distributed in the hope that it will be useful,
-#    but WITHOUT ANY WARRANTY; without even the implied warranty of
-#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-#    GNU Affero General Public License for more details.
-#
-#    You should have received a copy of the GNU Affero General Public License
-#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
-#
-##############################################################################
+# Copyright (C) 2010 Savoir-faire Linux (<http://www.savoirfairelinux.com>).
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+
 {
 <<<<<<< 175f2f66ce72543a357e8fd8e89b4ba0f9e8a52c
 <<<<<<< a2fe05867a1faacf29508c3abc1f0c8b98285c87
@@ -48,7 +32,7 @@ This module enables you to manage reviews of your management system.
     """,
 =======
     "name": "Management System - Review",
-    "version": "9.0.1.0.0",
+    "version": "10.0.1.0.0",
     "author": "Savoir-faire Linux, Odoo Community Association (OCA)",
     "website": "http://www.savoirfairelinux.com",
     "license": "AGPL-3",
