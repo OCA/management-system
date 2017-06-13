f3c0c51d7696fff30f98c466989fc2523c79c98d
Eugen Don <eugendon87@gmail.com>
2017-06-13 09:23:28 +0200

    MIG mgmtsystem_review to V. 10.0

--- mgmtsystem_review/models/mgmtsystem_review_line.py
+++ mgmtsystem_review/models/mgmtsystem_review_line.py
@@ -1,25 +1,8 @@
 # -*- coding: utf-8 -*-
-##############################################################################
-#
-#    OpenERP, Open Source Management Solution
-#    Copyright (C) 2010 Savoir-faire Linux (<http://www.savoirfairelinux.com>).
-#
-#    This program is free software: you can redistribute it and/or modify
-#    it under the terms of the GNU Affero General Public License as
-#    published by the Free Software Foundation, either version 3 of the
-#    License, or (at your option) any later version.
-#
-#    This program is distributed in the hope that it will be useful,
-#    but WITHOUT ANY WARRANTY; without even the implied warranty of
-#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-#    GNU Affero General Public License for more details.
-#
-#    You should have received a copy of the GNU Affero General Public License
-#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
-#
-##############################################################################
+# Copyright (C) 2010 Savoir-faire Linux (<http://www.savoirfairelinux.com>).
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 
-from openerp import fields, models
+from odoo import fields, models
 
 
 class MgmtsystemReviewLine(models.Model):
@@ -36,17 +19,17 @@ class MgmtsystemReviewLine(models.Model):
     action_id = fields.Many2one(
         'mgmtsystem.action',
         'Action',
-        select=True)
+        index=True)
     nonconformity_id = fields.Many2one(
         'mgmtsystem.nonconformity',
         'Nonconformity',
-        select=True)
+        index=True)
     decision = fields.Text('Decision')
     review_id = fields.Many2one(
         'mgmtsystem.review',
         'Review',
         ondelete='cascade',
-        select=True)
+        index=True)
     company_id = fields.Many2one(
         'res.company',
         'Company',
