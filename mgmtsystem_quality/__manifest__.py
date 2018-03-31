2b5d4a6e03cb621f64dcbaa9b1edc7025d256fe1
Jose Maria Azaga <jose.alzaga@aselcis.com>
2018-03-31 03:42:09 +0200

    [11.0][MIG] mgmtsystem_quality: Migration to 11.0

---
+++ mgmtsystem_quality/__manifest__.py
@@ -0,0 +1,21 @@
+# Copyright (C) 2010 Savoir-faire Linux (<http://www.savoirfairelinux.com>).
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+
+{
+    "name": "Quality Management System",
+    "version": "11.0.1.0.0",
+    "author": "Savoir-faire Linux, Odoo Community Association (OCA)",
+    "website": "http://www.savoirfairelinux.com",
+    "license": "AGPL-3",
+    "category": "Management System",
+    "depends": [
+        'mgmtsystem_audit',
+        'document_page_quality_manual',
+        'mgmtsystem_review',
+    ],
+    "data": [
+        'data/mgmtsystem_system.xml',
+    ],
+    'installable': True,
+    "application": True,
+}
