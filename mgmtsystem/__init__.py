454d67a610a9f9b716923629dc308b4c1ea964f7
Jose Maria Alzaga <jose.alzaga@aselcis.com>
2018-04-06 19:17:50 +0200

    [MIG] mgmtsystem: Migration to 11.0

--- mgmtsystem/__init__.py
+++ mgmtsystem/__init__.py
@@ -1,8 +1,2 @@
-# -*- encoding: utf-8 -*-
-<<<<<<< c9a3eba175d74c79a93a10ed28643b85214dc452
-import mgmtsystem_system
-# vim:expandtab:smartindent:tabstop=4:softtabstop=4:shiftwidth=4:
-=======
 
-from . import mgmtsystem_system
->>>>>>> Ported mgmtsystem
+from . import models
