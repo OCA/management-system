c041e27c2311fd82501d029a17e8139027900224
Jose Maria Alzaga <jose.alzaga@aselcis.com>
2018-04-07 00:31:49 +0200

    [MIG] mgmtsystem_action: Migration to 11.0

--- mgmtsystem_action/__init__.py
+++ mgmtsystem_action/__init__.py
@@ -1,19 +1,5 @@
-# -*- encoding: utf-8 -*-
-<<<<<<< 2cb3e23cd6da406a2afd4eedfd7745ab01746e88
-import mgmtsystem_action
-# vim:expandtab:smartindent:tabstop=4:softtabstop=4:shiftwidth=4:
-=======
 
-<<<<<<< 2129e7ae751a8f47d8a71257da6b304537d1e081
-from . import mgmtsystem_action
-<<<<<<< 2c0f94ae423926902139b96d41a1f267eda3452d
->>>>>>> Ported mgmtsystem_action
-=======
-from . import mgmtsystem_action_stage
->>>>>>> Added stages to mgmtststem_action
-=======
 from . import (
     models,
     reports,
 )
->>>>>>> MIG mgmtsystem_action to V. 10.0
