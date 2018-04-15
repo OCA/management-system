d7ed98e0f6df587aa84a549304b68d495b72a9dc
Jose Maria Alzaga <jose.alzaga@aselcis.com>
2018-04-15 21:51:38 +0200

    [MIG] mgmtsystem_nonconformity: Migration to 11.0

--- mgmtsystem_nonconformity/models/__init__.py
+++ mgmtsystem_nonconformity/models/__init__.py
@@ -1,38 +1,10 @@
-# -*- coding: utf-8 -*-
-##############################################################################
-#
-#    OpenERP, Open Source Management Solution
-#    Copyright (C) 2013 Savoir-faire Linux (<http://www.savoirfairelinux.com>).
-#
-#    This program is free software: you can redistribute it and/or modify
-#    it under the terms of the GNU Affero General Public License as
-#    published by the Free Software Foundation, either version 3 of the
-#    License, or (at your option) any later version.
-#
-#    This program is distributed in the hope that it will be useful,
-#    but WITHOUT ANY WARRANTY; without even the implied warranty of
-#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-#    GNU Affero General Public License for more details.
-#
-#    You should have received a copy of the GNU Affero General Public License
-#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
-#
-##############################################################################
+# Copyright (C) 2010 Savoir-faire Linux (<http://www.savoirfairelinux.com>).
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 
-<<<<<<< 157e9a9e5bd2fcd6a75c3226eed6aa4b51c94cc2
-from . import (
-    mgmtsystem_nonconformity_state,
-    mgmtsystem_nonconformity_cause,
-    mgmtsystem_nonconformity_origin,
-    mgmtsystem_nonconformity_severity,
-    mgmtsystem_nonconformity,
-    mgmtsystem_action,
-)
-=======
+
 from . import mgmtsystem_nonconformity_stage
 from . import mgmtsystem_nonconformity_cause
 from . import mgmtsystem_nonconformity_origin
 from . import mgmtsystem_nonconformity_severity
 from . import mgmtsystem_nonconformity
 from . import mgmtsystem_action
->>>>>>> MIG-mgmtsystem_nonconformity-to-V.-10.0
