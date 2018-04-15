1fb09e00f936dfb41a2951a3cea52c0c94025a0b
Jose Maria Alzaga <jose.alzaga@aselcis.com>
2018-04-15 20:01:08 +0200

    [MIG] mgmtsystem_manual: Migration to 11.0

--- mgmtsystem_manual/__manifest__.py
+++ mgmtsystem_manual/__manifest__.py
@@ -1,10 +1,9 @@
-# -*- coding: utf-8 -*-
 # Copyright (C) 2010 Savoir-faire Linux (<http://www.savoirfairelinux.com>).
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 
 {
     "name": "Management System - Manual",
-    "version": "10.0.1.0.0",
+    "version": "11.0.1.0.0",
     "author": "Savoir-faire Linux,Odoo Community Association (OCA)",
     "website": "http://www.savoirfairelinux.com",
     "license": "AGPL-3",
