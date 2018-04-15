bb4d1529fc88cf43e11a731433539ebfebf0e48f
Jose Maria Alzaga <jose.alzaga@aselcis.com>
2018-04-15 20:12:03 +0200

    [MIG] document_page_quality_manual: Migration to 11.0

--- document_page_quality_manual/__manifest__.py
+++ document_page_quality_manual/__manifest__.py
@@ -1,10 +1,9 @@
-# -*- coding: utf-8 -*-
 # Copyright (C) 2004-2009 Tiny SPRL (<http://tiny.be>).
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 
 {
     'name': 'Quality Manual',
-    'version': '10.0.1.0.0',
+    'version': '11.0.1.0.0',
     'category': 'Management System',
     'author': "OpenERP SA, Odoo Community Association (OCA)",
     'website': 'http://odoo.com',
