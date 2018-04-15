55dea9d0e19439bc2453bd7192370ff7f818c4b6
Jose Maria Alzaga <jose.alzaga@aselcis.com>
2018-04-15 20:01:08 +0200

    [MIG] mgmtsystem_manual: Migration to 11.0

--- mgmtsystem_manual/README.rst
+++ mgmtsystem_manual/README.rst
@@ -59,6 +59,7 @@ Contributors
 * Savoir-faire Linux <support@savoirfairelinux.com>
 * Gervais Naoussi <gervaisnaoussi@gmail.com>
 * Eugen Don <eugen.don@don-systems.de>
+* Jose Maria Alzaga <jose.alzaga@aselcis.com>
 
 Maintainer
 ----------
--- mgmtsystem_manual/models/mgmtsystem_manual.py
+++ mgmtsystem_manual/models/mgmtsystem_manual.py
@@ -1,4 +1,3 @@
-# -*- coding: utf-8 -*-
 
 from odoo import models, fields
 
@@ -7,4 +6,5 @@ class MgmtSystemManual(models.Model):
 
     _inherit = 'mgmtsystem.system'
     _description = 'Manual'
-    manual = fields.Many2one('document.page', string='Manual')
+    manual_id = fields.Many2one('document.page', string='Manual',
+                                oldname='manual')
