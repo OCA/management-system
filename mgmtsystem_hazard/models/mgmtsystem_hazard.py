7a2526a234297bc0e5902021678807de52a44af8
Nadege <nadege.grandjean@logicasoft.eu>
2018-08-01 15:15:34 +0200

    [MIG] mgmtsystem_hazard: Migration to 10.0
    [MIG] mgmtsystem_hazard: Migration to 10.0
    
    [MIG] mgmtsystem_hazard: Migration to 10.0

--- mgmtsystem_hazard/models/mgmtsystem_hazard.py
+++ mgmtsystem_hazard/models/mgmtsystem_hazard.py
@@ -2,7 +2,7 @@
 # Copyright (C) 2010 Savoir-faire Linux (<http://www.savoirfairelinux.com>).
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 
-from openerp import models, fields
+from odoo import models, fields
 
 
 class MgmtsystemHazard(models.Model):
@@ -10,6 +10,7 @@ class MgmtsystemHazard(models.Model):
 
     _name = "mgmtsystem.hazard"
     _description = __doc__
+    _inherit = ['mail.thread']
 
     name = fields.Char('Name', required=True, translate=True)
     type_id = fields.Many2one(
