a78b886cbeeba67a89810ee4b55f9f965995a03e
Nadege <nadege.grandjean@logicasoft.eu>
2018-08-13 13:29:01 +0200

    mgmtsystem_hazard_risk : use safe_eval to parse risk formula

--- mgmtsystem_hazard_risk/models/common.py
+++ mgmtsystem_hazard_risk/models/common.py
@@ -2,8 +2,10 @@
 # Copyright (C) 2010 Savoir-faire Linux (<http://www.savoirfairelinux.com>).
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 
+from odoo.tools.safe_eval import safe_eval
+
 
 def _parse_risk_formula(formula, a, b, c):
     """Calculate the risk replacing the variables A, B, C into the formula."""
     f = formula.replace('A', str(a)).replace('B', str(b)).replace('C', str(c))
-    return eval(f)
+    return safe_eval(f)
