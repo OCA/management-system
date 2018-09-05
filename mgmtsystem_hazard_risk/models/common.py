19f55e1de6d8d4641b5d6030a921e6b33c2f4fbb
Nadege <nadege.grandjean@logicasoft.eu>
2018-09-05 11:52:43 +0200

    [MIG] mgmtsystem_hazard_risk: Migration to 10.0

--- mgmtsystem_hazard_risk/models/common.py
+++ mgmtsystem_hazard_risk/models/common.py
@@ -1,26 +1,11 @@
-# -*- encoding: utf-8 -*-
-##############################################################################
-#
-#    OpenERP, Open Source Management Solution
-#    Copyright (C) 2010 Savoir-faire Linux (<http://www.savoirfairelinux.com>).
-#
-#    This program is free software: you can redistribute it and/or modify
-#    it under the terms of the GNU Affero General Public License as
-#    published by the Free Software Foundation, either version 3 of the
-#    License, or (at your option) any later version.
-#
-#    This program is distributed in the hope that it will be useful,
-#    but WITHOUT ANY WARRANTY; without even the implied warranty of
-#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-#    GNU Affero General Public License for more details.
-#
-#    You should have received a copy of the GNU Affero General Public License
-#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
-#
-##############################################################################
+# -*- coding: utf-8 -*-
+# Copyright (C) 2010 Savoir-faire Linux (<http://www.savoirfairelinux.com>).
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+
+from odoo.tools.safe_eval import safe_eval
 
 
 def _parse_risk_formula(formula, a, b, c):
     """Calculate the risk replacing the variables A, B, C into the formula."""
     f = formula.replace('A', str(a)).replace('B', str(b)).replace('C', str(c))
-    return eval(f)
+    return safe_eval(f)
