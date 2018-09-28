8db286a7ffaf9d0122b9ef1bc1df188536a2c68f
Jose Maria Alzaga <jose.alzaga@aselcis.com>
2018-09-28 05:13:42 +0200

    [11.0][MIG] mgmtsystem_manual
    * change field model by model_id
    
    * add oldname
    
    * remove old migration scripts
    
    * fix line too long

--- mgmtsystem_manual/models/mgmtsystem_manual.py
+++ mgmtsystem_manual/models/mgmtsystem_manual.py
@@ -6,4 +6,5 @@ class MgmtSystemManual(models.Model):
 
     _inherit = 'mgmtsystem.system'
     _description = 'Manual'
-    manual = fields.Many2one('document.page', string='Manual')
+    manual_id = fields.Many2one('document.page', string='Manual',
+                                oldname='manual')
