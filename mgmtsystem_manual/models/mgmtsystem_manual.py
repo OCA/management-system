821c891729c5ac72e088012ce12fb64e9aa111b4
Jose Maria Alzaga <jose.alzaga@aselcis.com>
2018-09-28 05:13:42 +0200

    [11.0][MIG] mgmtsystem_manual: change field model by model_id

--- mgmtsystem_manual/models/mgmtsystem_manual.py
+++ mgmtsystem_manual/models/mgmtsystem_manual.py
@@ -6,4 +6,5 @@ class MgmtSystemManual(models.Model):
 
     _inherit = 'mgmtsystem.system'
     _description = 'Manual'
-    manual = fields.Many2one('document.page', string='Manual')
+    manual_id = fields.Many2one('document.page', string='Manual')
+# old name of manual_id was manual
