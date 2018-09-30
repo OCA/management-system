25d8173ee1f2121c04985cd5955b192a1019854d
Jose Maria Alzaga <jose.alzaga@aselcis.com>
2018-09-30 21:38:26 +0200

    [11.0][MIG] mgmtsystem_manual: add oldname

--- mgmtsystem_manual/models/mgmtsystem_manual.py
+++ mgmtsystem_manual/models/mgmtsystem_manual.py
@@ -6,5 +6,4 @@ class MgmtSystemManual(models.Model):
 
     _inherit = 'mgmtsystem.system'
     _description = 'Manual'
-    manual_id = fields.Many2one('document.page', string='Manual')
-# old name of manual_id was manual
+    manual_id = fields.Many2one('document.page', string='Manual', oldname='manual')
