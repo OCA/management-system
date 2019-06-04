25c806b89afc2bf1e1b9aa7ae655465447a136b7
Simone Orsi <simahawk@gmail.com>
2019-06-04 18:29:03 +0200

    [MIG][12.0] mgmtsystem base

--- mgmtsystem/models/mgmtsystem_system.py
+++ mgmtsystem/models/mgmtsystem_system.py
@@ -4,15 +4,14 @@
 from odoo import models, fields
 
 
-def own_company(self):
-    return self.env.user.company_id.id
-
-
 class MgmtSystemSystem(models.Model):
 
     _name = 'mgmtsystem.system'
     _description = 'System'
 
     name = fields.Char('System', required=True)
-    company_id = fields.Many2one('res.company', 'Company',
-                                 default=own_company)
+    company_id = fields.Many2one(
+        'res.company',
+        'Company',
+        default=lambda self: self.env.user.company_id.id,
+    )
