5cfe3d74deec794e07796d3f97321f7f6eb4e255
Iryna Vyshnevska <i.vyshnevska@mobilunity.com>
2019-06-05 12:08:55 +0300

    [MIG] document_page_procedure: migrate to v12

--- document_page_procedure/__manifest__.py
+++ document_page_procedure/__manifest__.py
@@ -3,7 +3,7 @@
 
 {
     "name": "Document Management - Wiki - Procedures",
-    "version": "11.0.1.0.0",
+    "version": "12.0.1.0.0",
     "author": "Savoir-faire Linux,Odoo Community Association (OCA)",
     "website": "https://github.com/OCA/management-system",
     "license": "AGPL-3",
