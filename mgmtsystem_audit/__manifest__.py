3a17447de5e87c83976fb6d988f97d4f63c15935
ernesto <ernesto.tejeda87@gmail.com>
2019-07-24 12:43:27 -0400

    [MIG] mgmtsystem_audit: Migration to 12.0

--- mgmtsystem_audit/__manifest__.py
+++ mgmtsystem_audit/__manifest__.py
@@ -1,14 +1,13 @@
-# -*- coding: utf-8 -*-
 # Copyright (C) 2010 Savoir-faire Linux (<http://www.savoirfairelinux.com>).
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 {
     "name": "Management System - Audit",
-    "version": "10.0.1.0.1",
+    "version": "12.0.1.0.0",
     "author": "Savoir-faire Linux, Odoo Community Association (OCA)",
-    "website": "http://www.savoirfairelinux.com",
+    "website": "https://github.com/OCA/management-system",
     "license": "AGPL-3",
     "category": "Management System",
-    "depends": ['mgmtsystem_nonconformity', 'base_action_rule'],
+    "depends": ['mgmtsystem_nonconformity', 'base_automation'],
     "data": [
         'security/ir.model.access.csv',
         'security/mgmtsystem_audit_security.xml',
---
+++ mgmtsystem_audit/readme/USAGE.rst
@@ -0,0 +1,13 @@
+To use this module, you need to:
+
+* go to Management Systems > Audits
+* create a new audit
+* fill up its name, its auditors and schedule the date
+* prepare your questions with the verification list and print it
+* drive the audit and log answers in your verification list
+* finish your audit by writing the strong points, points to improve and creating improvements opportunities and nonconformities
+* print the audit report and close the audit
+
+For further information, please visit:
+
+* http://fr.slideshare.net/max3903/iso-anmanagement-systemswithopenerpen
