74f0c2caf01caeaeb8a0e24c40ba108fc3d531c3
Pedro M. Baeza <pedro.baeza@tecnativa.com>
2019-09-04 12:46:55 +0200

    [FIX] mgmtsystem_nonconformity: Proper script
    Without decorator, you receive a cursor, not an environment

--- mgmtsystem_nonconformity/migrations/12.0.1.0.0/post-migrate.py
+++ mgmtsystem_nonconformity/migrations/12.0.1.0.0/post-migrate.py
@@ -1,8 +1,10 @@
 # -*- coding: utf-8 -*-
 # Copyright 2019 Alexandre Fayolle <alexandre.fayolle@camptocamp.com>
 # License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
+from openupgradelib import openupgrade
 
 
+@openupgrade.migrate()
 def migrate(env, version):
     """Update database from previous versions, after updating module."""
     env['mgmtsystem.nonconformity.cause']._parent_store_compute()
