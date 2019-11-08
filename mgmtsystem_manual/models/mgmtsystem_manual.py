ebf550113e302e76cf0fa7e1f6fb407543f5900d
ernesto <ernesto.tejeda87@gmail.com>
2019-11-08 12:39:05 -0500

    [IMP] mgmtsystem_manual: black, isort

--- mgmtsystem_manual/models/mgmtsystem_manual.py
+++ mgmtsystem_manual/models/mgmtsystem_manual.py
@@ -1,13 +1,10 @@
 # Copyright 2019 Odoo Community Association
 # License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
 
-from odoo import models, fields
+from odoo import fields, models
 
 
 class MgmtSystemManual(models.Model):
-    _inherit = 'mgmtsystem.system'
+    _inherit = "mgmtsystem.system"
 
-    manual_id = fields.Many2one(
-        'document.page',
-        string='Manual'
-    )
+    manual_id = fields.Many2one("document.page", string="Manual")
