513a2f0421c18fb97a6811742ad32b80b1e6ae97
ernesto <ernesto.tejeda87@gmail.com>
2019-11-08 10:44:10 -0500

    [IMP] mgmtsystem: black, isort

--- mgmtsystem/tests/test_create_mgmtsystem.py
+++ mgmtsystem/tests/test_create_mgmtsystem.py
@@ -5,11 +5,8 @@ from odoo.tests import common
 
 
 class TestModelAction(common.TransactionCase):
-
     def test_create_system(self):
-        record = self.env['mgmtsystem.system'].create({
-            "name": "SampleSystem",
-        })
+        record = self.env["mgmtsystem.system"].create({"name": "SampleSystem"})
 
         self.assertEqual(record.name, "SampleSystem")
         self.assertEqual(record.company_id.id, self.env.user.company_id.id)
