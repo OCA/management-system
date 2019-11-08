ea1463a9f06f5c4dac41879f96ae22050a510de7
ernesto <ernesto.tejeda87@gmail.com>
2019-11-08 15:51:29 -0500

    [MIG] mgmtsystem_survey: Migration to 13.0

--- mgmtsystem_survey/__manifest__.py
+++ mgmtsystem_survey/__manifest__.py
@@ -3,13 +3,12 @@
 
 {
     "name": "Management System - Survey",
-    "version": "12.0.1.0.0",
+    "version": "13.0.1.0.0",
     "author": "Savoir-faire Linux,Odoo Community Association (OCA)",
     "website": "https://github.com/OCA/management-system",
     "license": "AGPL-3",
     "category": "Management System",
     "depends": ["mgmtsystem", "survey"],
-    "data": ["data/survey_stage.xml", "views/survey_survey.xml"],
-    "demo": [],
+    "data": ["views/survey_survey.xml"],
     "installable": True,
 }
