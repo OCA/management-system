f74c8a592d42071cf97680250425730115753465
ernesto <ernesto.tejeda87@gmail.com>
2019-11-12 11:51:21 -0500

    [MIG] document_page_work_instruction: Migration to 13.0

--- document_page_work_instruction/__manifest__.py
+++ document_page_work_instruction/__manifest__.py
@@ -3,13 +3,12 @@
 
 {
     "name": "Document Management - Wiki - Work Instructions",
-    "version": "12.0.1.0.0",
+    "version": "13.0.1.0.0",
     "author": "Savoir-faire Linux,Odoo Community Association (OCA)",
     "website": "https://github.com/OCA/management-system",
     "license": "AGPL-3",
-    "category": "Generic Modules/Others",
+    "category": "Management System",
     "depends": ["document_page", "mgmtsystem"],
     "data": ["data/document_page.xml", "views/document_page_work_instructions.xml"],
-    "demo": [],
     "installable": True,
 }
