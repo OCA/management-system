7ec1825b35c63980f2ea33ed025d1d81c30e92ba
ernestotejeda <ernesto.tejeda87@gmail.com>
2019-11-26 12:40:53 -0500

    [IMP] mgmtsystem_nonconformity: black, isort

--- mgmtsystem_nonconformity/tests/test_cause.py
+++ mgmtsystem_nonconformity/tests/test_cause.py
@@ -19,12 +19,12 @@
 ##############################################################################
 
 from contextlib import contextmanager
-from odoo.tests import common
+
 from odoo import exceptions
+from odoo.tests import common
 
 
 class TestModelCause(common.TransactionCase):
-
     @contextmanager
     def assertRaisesRollback(self, *args, **kwargs):
         """Do a regular assertRaises but perform rollback at the end
@@ -35,38 +35,36 @@ class TestModelCause(common.TransactionCase):
 
     def test_create_cause(self):
 
-        record = self.env['mgmtsystem.nonconformity.cause'].create({
-            "name": "TestCause",
-        })
+        record = self.env["mgmtsystem.nonconformity.cause"].create(
+            {"name": "TestCause"}
+        )
 
         self.assertNotEqual(record.id, 0)
         self.assertNotEqual(record.id, None)
 
     def test_name_get(self):
 
-        record = self.env['mgmtsystem.nonconformity.cause'].create({
-            "name": "TestCause",
-        })
+        record = self.env["mgmtsystem.nonconformity.cause"].create(
+            {"name": "TestCause"}
+        )
 
         name_assoc = record.name_get()
 
         self.assertEqual(name_assoc[0][1], "TestCause")
         self.assertEqual(name_assoc[0][0], record.id)
 
-        record2 = self.env['mgmtsystem.nonconformity.cause'].create({
-            "name": "test2",
-            "parent_id": record.id
-        })
+        record2 = self.env["mgmtsystem.nonconformity.cause"].create(
+            {"name": "test2", "parent_id": record.id}
+        )
 
         name_assoc = record2.name_get()
 
         self.assertEqual(name_assoc[0][1], "TestCause / test2")
         self.assertEqual(name_assoc[0][0], record2.id)
 
-        record3 = self.env['mgmtsystem.nonconformity.cause'].create({
-            "name": "test3",
-            "parent_id": record2.id
-        })
+        record3 = self.env["mgmtsystem.nonconformity.cause"].create(
+            {"name": "test3", "parent_id": record2.id}
+        )
 
         name_assoc = record3.name_get()
 
@@ -74,12 +72,11 @@ class TestModelCause(common.TransactionCase):
         self.assertEqual(name_assoc[0][0], record3.id)
 
     def test_recursion(self):
-        parent = self.env['mgmtsystem.nonconformity.cause'].create({
-            "name": "ParentCause",
-        })
-        child = self.env['mgmtsystem.nonconformity.cause'].create({
-            "name": "ChildCause",
-            "parent_id": parent.id,
-        })
+        parent = self.env["mgmtsystem.nonconformity.cause"].create(
+            {"name": "ParentCause"}
+        )
+        child = self.env["mgmtsystem.nonconformity.cause"].create(
+            {"name": "ChildCause", "parent_id": parent.id}
+        )
         with self.assertRaisesRollback(exceptions.ValidationError):
             parent.write({"parent_id": child.id})
--- mgmtsystem_nonconformity/tests/test_origin.py
+++ mgmtsystem_nonconformity/tests/test_origin.py
@@ -23,38 +23,36 @@ from odoo.tests import common
 class TestModelOrigin(common.TransactionCase):
     def test_create_origin(self):
 
-        record = self.env['mgmtsystem.nonconformity.origin'].create({
-            "name": "TestOrigin",
-        })
+        record = self.env["mgmtsystem.nonconformity.origin"].create(
+            {"name": "TestOrigin"}
+        )
 
         self.assertNotEqual(record.id, 0)
         self.assertNotEqual(record.id, None)
 
     def test_name_get(self):
 
-        record = self.env['mgmtsystem.nonconformity.origin'].create({
-            "name": "TestOrigin",
-        })
+        record = self.env["mgmtsystem.nonconformity.origin"].create(
+            {"name": "TestOrigin"}
+        )
 
         name_assoc = record.name_get()
 
         self.assertEqual(name_assoc[0][1], "TestOrigin")
         self.assertEqual(name_assoc[0][0], record.id)
 
-        record2 = self.env['mgmtsystem.nonconformity.origin'].create({
-            "name": "test2",
-            "parent_id": record.id
-        })
+        record2 = self.env["mgmtsystem.nonconformity.origin"].create(
+            {"name": "test2", "parent_id": record.id}
+        )
 
         name_assoc = record2.name_get()
 
         self.assertEqual(name_assoc[0][1], "TestOrigin / test2")
         self.assertEqual(name_assoc[0][0], record2.id)
 
-        record3 = self.env['mgmtsystem.nonconformity.origin'].create({
-            "name": "test3",
-            "parent_id": record2.id
-        })
+        record3 = self.env["mgmtsystem.nonconformity.origin"].create(
+            {"name": "test3", "parent_id": record2.id}
+        )
 
         name_assoc = record3.name_get()
 
