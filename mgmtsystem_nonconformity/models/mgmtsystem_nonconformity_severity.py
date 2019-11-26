1b980153eced837dbba901b1bfa53d593ceaa508
ernestotejeda <ernesto.tejeda87@gmail.com>
2019-11-26 12:40:53 -0500

    [IMP] mgmtsystem_nonconformity: black, isort

--- mgmtsystem_nonconformity/models/mgmtsystem_nonconformity_severity.py
+++ mgmtsystem_nonconformity/models/mgmtsystem_nonconformity_severity.py
@@ -1,7 +1,7 @@
 # Copyright (C) 2010 Savoir-faire Linux (<http://www.savoirfairelinux.com>).
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 
-from odoo import models, fields
+from odoo import fields, models
 
 
 class MgmtsystemNonconformitySeverity(models.Model):
@@ -12,6 +12,6 @@ class MgmtsystemNonconformitySeverity(models.Model):
     _description = "Severity of Complaints and Nonconformities"
 
     name = fields.Char("Title", required=True, translate=True)
-    sequence = fields.Integer('Sequence')
-    description = fields.Text('Description', translate=True)
-    active = fields.Boolean('Active?', default=True)
+    sequence = fields.Integer("Sequence")
+    description = fields.Text("Description", translate=True)
+    active = fields.Boolean("Active?", default=True)
