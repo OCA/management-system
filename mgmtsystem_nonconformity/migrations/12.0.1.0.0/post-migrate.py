6ca214fd3d3e94c47d69256713af5d845df0e28c
ernestotejeda <ernesto.tejeda87@gmail.com>
2019-11-26 12:40:53 -0500

    [IMP] mgmtsystem_nonconformity: black, isort

--- mgmtsystem_nonconformity/migrations/12.0.1.0.0/post-migrate.py
+++ mgmtsystem_nonconformity/migrations/12.0.1.0.0/post-migrate.py
@@ -1,4 +1,3 @@
-# -*- coding: utf-8 -*-
 # Copyright 2019 Alexandre Fayolle <alexandre.fayolle@camptocamp.com>
 # License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
 from openupgradelib import openupgrade
@@ -7,5 +6,5 @@ from openupgradelib import openupgrade
 @openupgrade.migrate()
 def migrate(env, version):
     """Update database from previous versions, after updating module."""
-    env['mgmtsystem.nonconformity.cause']._parent_store_compute()
-    env['mgmtsystem.nonconformity.origin']._parent_store_compute()
+    env["mgmtsystem.nonconformity.cause"]._parent_store_compute()
+    env["mgmtsystem.nonconformity.origin"]._parent_store_compute()
