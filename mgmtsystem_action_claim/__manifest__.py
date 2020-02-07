f54773ea10e7e2fc3c30973c274d8ede8692f4da
ludolelarge <52041088+ludolelarge@users.noreply.github.com>
2020-02-07 18:28:01 +0100

    Add files via upload

---
+++ mgmtsystem_action_claim/__manifest__.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+
+{
+	"name": "Management System - Action - Claim",
+    "version": "1.0",
+    "author" : "Ludovic Lelarge",
+    "website": "www.eta123.be",
+    'license': 'LGPL-3',
+    "category": "Management System",
+    'description': """
+        Link Mgmtsystem Action - Claim.
+    """,
+    'depends': ['mgmtsystem_action','mgmtsystem_claim'],
+    'data': [
+        'views/mgmtsystem_action_views.xml',
+        'views/mgmtsystem_claim_views.xml'
+    ],
+    'installable': True,
+}
