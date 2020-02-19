d55670a9dca04302074ebcc57917b91e0717fce4
ludolelarge <52041088+ludolelarge@users.noreply.github.com>
2020-02-19 09:23:53 +0100

    Update __manifest__.py

--- mgmtsystem_action_claim/__manifest__.py
+++ mgmtsystem_action_claim/__manifest__.py
@@ -1,11 +1,12 @@
-# -*- coding: utf-8 -*-
+# Copyright (C) 2018 - 2020 Ludovic Lelarge (<http://www.eta123.be>).
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 
 {
     "name": "Management System - Action - Claim",
     "version": "13.0.1.0.0",
     "author": "Ludovic Lelarge, Odoo Community Association (OCA)",
     "website": "www.eta123.be",
-    "license": "LGPL-3",
+    "license": "AGPL-3",
     "category": "Management System",
     "depends": ["mgmtsystem_action","mgmtsystem_claim"],
     "data": [
