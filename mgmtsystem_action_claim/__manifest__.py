8263cb31398360343fee5be3ac46458144736498
ludolelarge <52041088+ludolelarge@users.noreply.github.com>
2020-02-19 07:53:55 +0100

    Update __manifest__.py

--- mgmtsystem_action_claim/__manifest__.py
+++ mgmtsystem_action_claim/__manifest__.py
@@ -2,7 +2,7 @@
 
 {
 	"name": "Management System - Action - Claim",
-    "version": "13.0",
+    "version": "13.0.1.0.0",
     "author" : "Ludovic Lelarge",
     "website": "www.eta123.be",
     'license': 'LGPL-3',
