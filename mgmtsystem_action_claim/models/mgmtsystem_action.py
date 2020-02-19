8e7c6c17d6d4f3e54a2c2230d7dd1a17db5d499e
ludolelarge <52041088+ludolelarge@users.noreply.github.com>
2020-02-19 09:29:58 +0100

    Update mgmtsystem_action.py

--- mgmtsystem_action_claim/models/mgmtsystem_action.py
+++ mgmtsystem_action_claim/models/mgmtsystem_action.py
@@ -1,26 +1,47 @@
-# -*- coding: utf-8 -*-
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 
-from odoo import fields,models
+from odoo import fields, models
 
 class MgmtsystemAction(models.Model):
-    _name = 'mgmtsystem.action'
-    _inherit = ['mgmtsystem.action']
+    _name = "mgmtsystem.action"
+    _inherit = ["mgmtsystem.action"]
          
     def _compute_mgmtsystem_claim_count(self):
         for mgmtsystem_action in self:
-            mgmtsystem_action.mgmtsystem_claim_count = len(mgmtsystem_action.mgmtsystem_claim_ids)
+            mgmtsystem_action.mgmtsystem_claim_count = len(
+                mgmtsystem_action.mgmtsystem_claim_ids
+            )
     
-    mgmtsystem_claim_ids = fields.Many2many('mgmtsystem.claim','mgmtsystem_action_mgmtsystem_claim_rel','mgmtsystem_action_id','mgmtsystem_claim_id','Claim Ids')
-    mgmtsystem_claim_count = fields.Integer(compute='_compute_mgmtsystem_claim_count', string="Number of claim")
+    mgmtsystem_claim_ids = fields.Many2many(
+        "mgmtsystem.claim",
+        "mgmtsystem_action_mgmtsystem_claim_rel",
+        "mgmtsystem_action_id",
+        "mgmtsystem_claim_id",
+        "Claim Ids"
+    )
+    mgmtsystem_claim_count = fields.Integer(
+        compute="_compute_mgmtsystem_claim_count", string="Number of claim"
+    )
     
 class MgmtsystemClaim(models.Model):
-    _name = 'mgmtsystem.claim'
-    _inherit = ['mgmtsystem.claim']
+    _name = "mgmtsystem.claim"
+    _inherit = ["mgmtsystem.claim"]
 
     def _compute_mgmtsystem_action_count(self):
         for mgmtsystem_claim in self:
-            mgmtsystem_claim.mgmtsystem_action_count = len(mgmtsystem_claim.mgmtsystem_action_ids)
+            mgmtsystem_claim.mgmtsystem_action_count = len(
+                mgmtsystem_claim.mgmtsystem_action_ids
+            )
     
-    mgmtsystem_action_ids = fields.Many2many('mgmtsystem.action','mgmtsystem_action_mgmtsystem_claim_rel','mgmtsystem_claim_id','mgmtsystem_action_id','Action Ids')
-    mgmtsystem_action_count = fields.Integer(compute='_compute_mgmtsystem_action_count', string="Number of action")
+    mgmtsystem_action_ids = fields.Many2many(
+        "mgmtsystem.action",
+        "mgmtsystem_action_mgmtsystem_claim_rel",
+        "mgmtsystem_claim_id",
+        "mgmtsystem_action_id",
+        "Action Ids"
+    )
+    mgmtsystem_action_count = fields.Integer(
+        compute="_compute_mgmtsystem_action_count",
+        string="Number of action"
+    )
     
