f14ffefbd95bc91771aa748c76f9e4a9f05ab894
flachica <flachica@gitlab.freyi.es>
2020-02-26 15:41:03 +0100

    [IMP] gmtsystem_audit: black, isort

--- mgmtsystem_audit/tests/test_copy_verification_lines.py
+++ mgmtsystem_audit/tests/test_copy_verification_lines.py
@@ -8,19 +8,22 @@ class TestModelCopyVerificationLines(common.TransactionCase):
     """Test wizard's method."""
 
     def test_copyVerificationLines(self):
-        line_id = self.env["mgmtsystem.verification.line"].create({
-            "name": "What",
-            "procedure_id": self.env.ref("document_page.demo_page1").id
-        })
+        line_id = self.env["mgmtsystem.verification.line"].create(
+            {
+                "name": "What",
+                "procedure_id": self.env.ref("document_page.demo_page1").id,
+            }
+        )
 
         src_record = self.env.ref("mgmtsystem_audit.mgmtsystem_audit_demo")
         src_record.line_ids = [line_id.id]
         dest_record = self.env.ref("mgmtsystem_audit.mgmtsystem_audit_demo2")
-        copy_record = self.env["copy.verification.lines"].create({
-            "audit_src": src_record.id
-        })
+        copy_record = self.env["copy.verification.lines"].create(
+            {"audit_src": src_record.id}
+        )
         copy_record = copy_record.with_context(
-            active_id=dest_record.id, active_model="mgmtsystem.audit")
+            active_id=dest_record.id, active_model="mgmtsystem.audit"
+        )
 
         copy_record.copyVerificationLines()
 
