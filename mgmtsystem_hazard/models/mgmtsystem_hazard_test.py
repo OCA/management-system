5793915b08a16f2eae3c0f068df8ca6c8cb4d230
flachica <flachica@gitlab.freyi.es>
2020-03-10 17:30:37 +0100

    [IMP] mgmtsystem_hazard: black, isort

--- mgmtsystem_hazard/models/mgmtsystem_hazard_test.py
+++ mgmtsystem_hazard/models/mgmtsystem_hazard_test.py
@@ -1,25 +1,17 @@
 # Copyright (C) 2010 Savoir-faire Linux (<http://www.savoirfairelinux.com>).
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 
-from odoo import models, fields
+from odoo import fields, models
 
 
 class MgmtsystemHazardTest(models.Model):
     _name = "mgmtsystem.hazard.test"
     _description = "Implementation Tests of hazard"
 
-    name = fields.Char('Test', required=True, translate=True)
-    responsible_user_id = fields.Many2one(
-        'res.users',
-        'Responsible',
-        required=True,
-    )
-    review_date = fields.Date('Review Date', required=True)
-    executed = fields.Boolean('Executed')
+    name = fields.Char("Test", required=True, translate=True)
+    responsible_user_id = fields.Many2one("res.users", "Responsible", required=True)
+    review_date = fields.Date("Review Date", required=True)
+    executed = fields.Boolean("Executed")
     hazard_id = fields.Many2one(
-        'mgmtsystem.hazard',
-        'Hazard',
-        ondelete='cascade',
-        required=False,
-        index=True,
+        "mgmtsystem.hazard", "Hazard", ondelete="cascade", required=False, index=True
     )
