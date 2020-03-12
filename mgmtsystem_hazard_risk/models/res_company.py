cecef7d85efd820fdad259df18baf1c651ed9170
flachica <flachica@gitlab.freyi.es>
2020-03-12 17:35:44 +0100

    [MIG] mgmsystem_hazard_risk: Migration to 13.0

---
+++ mgmtsystem_hazard_risk/models/res_company.py
@@ -0,0 +1,10 @@
+# Copyright (C) 2020 Guadaltech Soluciones Tecnológicas (<http://www.guadaltech.es>).
+# Part of Odoo. See LICENSE file for full copyright and licensing details.
+
+from odoo import models, fields
+
+
+class ResCompany(models.Model):
+    _inherit = 'res.company'
+
+    risk_computation_id = fields.Many2one('mgmtsystem.hazard.risk.computation', string='Risk Computation')
---
+++ mgmtsystem_hazard_risk/readme/CONTRIBUTORS.rst
@@ -0,0 +1,4 @@
+* Maxime Chambreuil <maxime.chambreuil@savoirfairelinux.com> 
+* Loïc Lacroix <loic.lacroix@savoirfairelinux.com>
+* Nadège Grandjean <nadege.grandjean@outlook.be>
+* Fernando La Chica <fernando.lachica@guadaltech.es>
