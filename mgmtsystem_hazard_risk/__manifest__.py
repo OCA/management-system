473f11f0705dbe43cedc16d22def11387131234e
flachica <flachica@gitlab.freyi.es>
2020-03-13 10:53:10 +0100

    [MIG] mgmtsystem_hazard_risk: Migration to 13.0

--- mgmtsystem_hazard_risk/__manifest__.py
+++ mgmtsystem_hazard_risk/__manifest__.py
@@ -1,9 +1,8 @@
-# -*- coding: utf-8 -*-
 # Copyright (C) 2010 Savoir-faire Linux (<http://www.savoirfairelinux.com>).
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 {
     "name": "Hazard Risk",
-    "version": "10.0.1.1.0",
+    "version": "13.0.1.0.0",
     "author": "Savoir-faire Linux, Odoo Community Association (OCA)",
     "website": "http://www.savoirfairelinux.com",
     "license": "AGPL-3",
@@ -17,11 +16,11 @@
         'security/mgmtsystem_hazard_security.xml',
         'data/mgmtsystem_hazard_risk_computation.xml',
         'data/mgmtsystem_hazard_risk_type.xml',
-        'views/res_company.xml',
         'views/mgmtsystem_hazard.xml',
         'views/mgmtsystem_hazard_risk_type.xml',
         'views/mgmtsystem_hazard_risk_computation.xml',
         'views/mgmtsystem_hazard_residual_risk.xml',
+        'views/res_config_settings_views.xml',
     ],
     "installable": True,
 }
--- mgmtsystem_hazard_risk/models/mgmtsystem_hazard.py
+++ mgmtsystem_hazard_risk/models/mgmtsystem_hazard.py
@@ -1,4 +1,3 @@
-# -*- coding: utf-8 -*-
 # Copyright (C) 2010 Savoir-faire Linux (<http://www.savoirfairelinux.com>).
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 
@@ -7,29 +6,27 @@ from .common import _parse_risk_formula
 
 
 class MgmtsystemHazard(models.Model):
-
-    _inherit = "mgmtsystem.hazard"
+    _inherit = 'mgmtsystem.hazard'
     risk_type_id = fields.Many2one(
         'mgmtsystem.hazard.risk.type',
         'Risk Type',
         required=True,
     )
-    risk = fields.Integer(compute="_compute_risk", string='Risk')
+    risk = fields.Integer(compute='_compute_risk', string='Risk')
     residual_risk_ids = fields.One2many(
         'mgmtsystem.hazard.residual_risk',
         'hazard_id',
         'Residual Risk Evaluations',
     )
 
-    @api.depends("probability_id", "severity_id", "usage_id")
+    @api.depends('probability_id', 'severity_id', 'usage_id')
     def _compute_risk(self):
-        mycompany = self.env['res.users'].browse(self._uid).company_id
         for hazard in self:
             if hazard.probability_id and \
-                    hazard.severity_id and\
-                    hazard.usage_id:
+                hazard.severity_id and \
+                hazard.usage_id:
                 hazard.risk = _parse_risk_formula(
-                    mycompany.risk_computation_id.name,
+                    self.env.company.risk_computation_id.name,
                     hazard.probability_id.value,
                     hazard.severity_id.value,
                     hazard.usage_id.value
