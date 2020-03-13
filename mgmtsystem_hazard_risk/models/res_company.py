be80f89be40656c0f9d20e6076f5d1dd2d4573a1
flachica <flachica@gitlab.freyi.es>
2020-03-13 10:53:10 +0100

    [MIG] mgmtsystem_hazard_risk: Migration to 13.0

--- mgmtsystem_hazard_risk/models/res_company.py
+++ mgmtsystem_hazard_risk/models/res_company.py
@@ -1,23 +1,10 @@
-# -*- coding: utf-8 -*-
-# Copyright (C) 2010 Savoir-faire Linux (<http://www.savoirfairelinux.com>).
-# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+# Copyright (C) 2020 Guadaltech Soluciones Tecnológicas (<http://www.guadaltech.es>).
+# Part of Odoo. See LICENSE file for full copyright and licensing details.
 
 from odoo import models, fields
 
 
 class ResCompany(models.Model):
+    _inherit = 'res.company'
 
-    _inherit = "res.company"
-
-    def _get_formula(self):
-        ids = self.env['mgmtsystem.hazard.risk.computation'].search(
-            [('name', '=', 'A * B * C')],
-            limit=1,
-        )
-        return ids
-
-    risk_computation_id = fields.Many2one(
-        'mgmtsystem.hazard.risk.computation',
-        'Risk Computation',
-        default=_get_formula
-    )
+    risk_computation_id = fields.Many2one('mgmtsystem.hazard.risk.computation', string='Risk Computation')
