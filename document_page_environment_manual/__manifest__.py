0372a09a3b4e36e8a3030e00dcf0ade1ccdf1a52
flachica <flachica@gitlab.freyi.es>
2020-04-02 16:47:44 +0200

    [IMP] document_page_environment_manual: black, isort

--- document_page_environment_manual/__manifest__.py
+++ document_page_environment_manual/__manifest__.py
@@ -1,6 +1,5 @@
-# -*- coding: utf-8 -*-
 ##############################################################################
-#    
+#
 #    OpenERP, Open Source Management Solution
 #    Copyright (C) 2010 Savoir-faire Linux (<http://www.savoirfairelinux.com>).
 #
@@ -20,17 +19,13 @@
 ##############################################################################
 {
     "name": "Document Management - Wiki - Environment Manual",
-    "version": "9.0.1.0.0",
+    "version": "13.0.1.0.0",
     "author": "Savoir-faire Linux,Odoo Community Association (OCA)",
     "website": "http://www.savoirfairelinux.com",
     "license": "AGPL-3",
     "category": "Generic Modules/Others",
-    "depends": [
-        'mgmtsystem_manual'
-    ],
-    "data": [
-        'data/document_page_environment_manual.xml'
-    ],
+    "depends": ["mgmtsystem_manual"],
+    "data": ["data/document_page_environment_manual.xml"],
     "demo": [],
-    'installable': False,
+    "installable": True,
 }
