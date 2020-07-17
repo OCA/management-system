ec0248bac164680c870440029df742507149fcab
flachica <fernandolachica@gmail.com>
2020-07-17 13:51:43 +0200

    [IMP] mgmtsystem_claim: black, isort

--- mgmtsystem_claim/__manifest__.py
+++ mgmtsystem_claim/__manifest__.py
@@ -1,4 +1,3 @@
-# -*- encoding: utf-8 -*-
 ##############################################################################
 #
 #    OpenERP, Open Source Management Solution
@@ -41,4 +40,3 @@
     ],
 }
 # vim:expandtab:smartindent:tabstop=4:softtabstop=4:shiftwidth=4:
-
