3f3c71533eb1829fb4c33386af19f011b096f9a4
dzung.tran <dungtd@trobz.com>
2021-02-22 16:02:31 +0700

    [MIG] document_page_quality_manual: Migration to 14.0

--- document_page_quality_manual/__manifest__.py
+++ document_page_quality_manual/__manifest__.py
@@ -3,7 +3,7 @@
 
 {
     "name": "Quality Manual",
-    "version": "13.0.1.0.0",
+    "version": "14.0.1.0.0",
     "category": "Management System",
     "author": "OpenERP SA, Odoo Community Association (OCA)",
     "website": "https://github.com/OCA/management-system",
