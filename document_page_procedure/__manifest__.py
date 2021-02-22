a6b9294bc5adfd25a757bbc26f25cf2a38450cc4
dzung.tran <dungtd@trobz.com>
2021-02-22 16:07:33 +0700

    [MIG] document_page_procedure: Migration to 14.0

--- document_page_procedure/__manifest__.py
+++ document_page_procedure/__manifest__.py
@@ -3,7 +3,7 @@
 
 {
     "name": "Document Management - Wiki - Procedures",
-    "version": "13.0.1.0.0",
+    "version": "14.0.1.0.0",
     "author": "Savoir-faire Linux, Odoo Community Association (OCA)",
     "website": "https://github.com/OCA/management-system",
     "license": "AGPL-3",
