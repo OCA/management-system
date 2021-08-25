6565fa04af5ae18820427be08d53f04e66f68d9e
Pedro M. Baeza <pedro.baeza@tecnativa.com>
2021-08-25 13:00:54 +0200

    [OU-FIX] mgmtsystem_survey: proper column in SQL
    The column was renamed in OpenUpgrade.

--- mgmtsystem_survey/migrations/13.0.1.0.0/pre-migration.py
+++ mgmtsystem_survey/migrations/13.0.1.0.0/pre-migration.py
@@ -2,18 +2,19 @@
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
 
 from openupgradelib import openupgrade
+from psycopg2 import sql
 
 
 @openupgrade.migrate()
 def migrate(env, version):
     openupgrade.logged_query(
         env.cr,
-        """
-        UPDATE survey_survey s
-        SET state = 'management_system'
-        FROM ir_model_data imd
-        WHERE imd.res_id = s.stage_id
+        sql.SQL(
+            """UPDATE survey_survey s
+            SET state = 'management_system'
+            FROM ir_model_data imd
+            WHERE imd.res_id = s.{}
             AND imd.module = 'mgmtsystem_survey'
-            AND imd.name = 'mgmtsystem_survey_type'
-        """,
+            AND imd.name = 'mgmtsystem_survey_type'"""
+        ).format(sql.Identifier(openupgrade.get_legacy_name("stage_id"))),
     )
