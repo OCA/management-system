10701e1f0af34936795dcc3d2a5bdd7762813054
aromera <aromera@pmsastro.es>
2022-01-10 14:33:34 +0100

    [MIG] mgmtsystem_claim: Migration to 14.0

--- mgmtsystem_claim/__manifest__.py
+++ mgmtsystem_claim/__manifest__.py
@@ -19,9 +19,9 @@
 ##############################################################################
 {
     "name": "Management System - Claim",
-    "version": "13.0.1.1.0",
+    "version": "14.0.1.0.0",
     "author": "Savoir-faire Linux,Odoo Community Association (OCA)",
-    "website": "http://www.savoirfairelinux.com",
+    "website": "https://github.com/OCA/management-system",
     "license": "AGPL-3",
     "category": "Management System",
     "depends": ["mgmtsystem", "crm_claim", "mail"],
