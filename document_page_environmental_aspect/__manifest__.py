4f5b55abc00507865d2fe0c8ff7749f62caf6d3d
aromera <aromera@pmsastro.es>
2022-01-10 14:13:05 +0100

    [MIG] document_page_environmental_aspect: Migration to 14.0

--- document_page_environmental_aspect/__manifest__.py
+++ document_page_environmental_aspect/__manifest__.py
@@ -20,9 +20,9 @@
 ##############################################################################
 {
     "name": "Environmental Aspects",
-    "version": "13.0.1.0.1",
+    "version": "14.0.1.0.0",
     "author": "Savoir-faire Linux, Odoo Community Association (OCA)",
-    "website": "http://www.savoirfairelinux.com",
+    "website": "https://github.com/OCA/management-system",
     "license": "AGPL-3",
     "category": "Generic Modules/Others",
     "depends": ["document_page", "mgmtsystem"],
