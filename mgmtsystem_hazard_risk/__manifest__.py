9d599468fe3193f761c9a9c8d3814dc435424f43
aromera <aromera@pmsastro.es>
2022-01-10 14:28:05 +0100

    [MIG] mgmtsystem_hazard_risk: Migration to 14.0

--- mgmtsystem_hazard_risk/__manifest__.py
+++ mgmtsystem_hazard_risk/__manifest__.py
@@ -2,9 +2,9 @@
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 {
     "name": "Hazard Risk",
-    "version": "13.0.1.0.0",
+    "version": "14.0.1.0.0",
     "author": "Savoir-faire Linux, Odoo Community Association (OCA)",
-    "website": "http://www.savoirfairelinux.com",
+    "website": "https://github.com/OCA/management-system",
     "license": "AGPL-3",
     "category": "Management System",
     "depends": ["mgmtsystem_hazard", "hr"],
