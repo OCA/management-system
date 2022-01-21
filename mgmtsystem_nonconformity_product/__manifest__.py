06f614757fb8f76374ff259117d321259f931f97
Stefano Consolaro <stefano.consolaro@mymage.it>
2022-01-21 21:23:14 +0100

    [IMP] mgmtsystem_nonconformity_product: black, isort, prettier

--- mgmtsystem_nonconformity_product/__manifest__.py
+++ mgmtsystem_nonconformity_product/__manifest__.py
@@ -3,23 +3,15 @@
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 
 {
-    "name":     "Management System - Nonconformity Product",
-    "summary":  "Bridge module between Product and Management System.",
-    "version":  "12.0.1.0.0",
+    "name": "Management System - Nonconformity Product",
+    "summary": "Bridge module between Product and Management System.",
+    "version": "13.0.1.0.0",
     "development_status": "Beta",
-
-    "author":   "Associazione PNLUG - Gruppo Odoo, Odoo Community Association (OCA)",
-    "website":  "https://gitlab.com/PNLUG/Odoo/management-system-improvements/tree/"
-                "11.0/mgmtsystem_extended",
-    "license":  "AGPL-3",
-
+    "author": "Associazione PNLUG - Gruppo Odoo, Odoo Community Association (OCA)",
+    "website": "https://odoo.pnlug.it",
+    "license": "AGPL-3",
     "category": "Management System",
-    "depends": [
-        'product',
-        'mgmtsystem_nonconformity',
-        ],
-    "data": [
-        'views/mgmtsystem_nonconformity_views.xml',
-        ],
-    'installable': True,
+    "depends": ["product", "mgmtsystem_nonconformity"],
+    "data": ["views/mgmtsystem_nonconformity_views.xml"],
+    "installable": True,
 }
