f483c9becfc5bf3111940545872ca98e8062d9e8
Stefano Consolaro <stefano.consolaro@mymage.it>
2022-05-23 21:14:57 +0200

    [MIG] mgmtsystem_action_efficacy: Migration to 14.0

--- mgmtsystem_action_efficacy/__manifest__.py
+++ mgmtsystem_action_efficacy/__manifest__.py
@@ -3,20 +3,15 @@
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl)
 
 {
-    "name":     "Management System - Action Efficacy",
-    "summary":  "Add information on the application of the Action.",
-    "version":  "12.0.1.0.0",
-    "development_status" : "Beta",
-
-    "author":   "Associazione PNLUG - Gruppo Odoo, Odoo Community Association (OCA)",
-
-    "website":  "https://github.com/OCA/management-system",
-    "license":  "AGPL-3",
-
+    "name": "Management System - Action Efficacy",
+    "summary": "Add information on the application of the Action.",
+    "version": "14.0.1.0.0",
+    "development_status": "Beta",
+    "author": "Associazione PNLUG - Gruppo Odoo, Odoo Community Association (OCA)",
+    "website": "https://github.com/OCA/management-system",
+    "license": "AGPL-3",
     "category": "Management System",
-    "depends": ['mgmtsystem_action',
-                ],
-    "data":    ['views/mgmtsystem_action_views.xml',
-                ],
-    'installable': True,
+    "depends": ["mgmtsystem_action"],
+    "data": ["views/mgmtsystem_action_views.xml"],
+    "installable": True,
 }
--- mgmtsystem_action_efficacy/models/__init__.py
+++ mgmtsystem_action_efficacy/models/__init__.py
@@ -1,2 +1,1 @@
-
 from . import mgmtsystem_action_efficacy
