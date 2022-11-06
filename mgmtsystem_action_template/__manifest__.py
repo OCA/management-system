8e5eedea8cd4347b91179db895249bd3972f217b
Stefano Consolaro <stefano.consolaro@mymage.it>
2022-11-06 21:16:39 +0100

    [MIG] mgmtsystem_action_template: Migration to 14.0

--- mgmtsystem_action_template/__manifest__.py
+++ mgmtsystem_action_template/__manifest__.py
@@ -4,7 +4,7 @@
 {
     "name": "Management System - Action Template",
     "summary": "Add Template management for Actions.",
-    "version": "13.0.1.1.0",
+    "version": "14.0.1.1.0",
     "author": "Associazione PNLUG - Gruppo Odoo, Odoo Community Association (OCA)",
     "website": "https://github.com/OCA/management-system",
     "license": "AGPL-3",
