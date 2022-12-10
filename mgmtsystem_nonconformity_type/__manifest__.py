8897c3e585778914f7b4f1d181cccd2d53665c4e
Stefano Consolaro <stefano.consolaro@mymage.it>
2022-12-10 09:39:04 +0100

    [MIG] mgmtsystem_nonconformity_type: Migration to 15.0

--- mgmtsystem_nonconformity_type/__manifest__.py
+++ mgmtsystem_nonconformity_type/__manifest__.py
@@ -3,7 +3,7 @@
 {
     "name": "Management System - Nonconformity Type",
     "summary": "Add Nonconformity classification for the root context.",
-    "version": "14.0.1.0.0",
+    "version": "15.0.1.0.0",
     "development_status": "Beta",
     "author": "Associazione PNLUG - Gruppo Odoo, Odoo Community Association (OCA)",
     "website": "https://github.com/OCA/management-system",
