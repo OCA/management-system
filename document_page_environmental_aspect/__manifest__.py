cda12dd6090d9ecffa7b367916110132d8d0fae7
FernandoRomera <fromera@astrohotel.es>
2022-12-13 11:41:11 +0100

    [16.0][MIG] document_page_environmental_aspect: Migration to 16.0

--- document_page_environmental_aspect/__manifest__.py
+++ document_page_environmental_aspect/__manifest__.py
@@ -20,7 +20,7 @@
 ##############################################################################
 {
     "name": "Environmental Aspects",
-    "version": "15.0.1.0.0",
+    "version": "16.0.1.0.0",
     "author": "Savoir-faire Linux, Odoo Community Association (OCA)",
     "website": "https://github.com/OCA/management-system",
     "license": "AGPL-3",
