0202a6373d71cfc5194b338ffd4e2844476bfe97
Rodrigo Neves <rodrigonevest@gmail.com>
2023-03-15 19:19:04 -0300

    [IMP] mgmtsystem_nonconformity_repair: black, isort, prettier

--- mgmtsystem_nonconformity_repair/models/mgmtsystem_nonconformity.py
+++ mgmtsystem_nonconformity_repair/models/mgmtsystem_nonconformity.py
@@ -5,7 +5,6 @@ from odoo import fields, models
 
 
 class MgmtsystemNonconformity(models.Model):
+    _inherit = "mgmtsystem.nonconformity"
 
-    _inherit = 'mgmtsystem.nonconformity'
-
-    repair_order_id = fields.Many2one('repair.order', 'Repair Order')
+    repair_order_id = fields.Many2one("repair.order", "Repair Order")
