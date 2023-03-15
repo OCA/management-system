837fca32515a223e746c9eab9e80b2b63bf8e869
Rodrigo Neves <rodrigonevest@gmail.com>
2023-03-15 19:19:04 -0300

    [IMP] mgmtsystem_nonconformity_repair: black, isort, prettier

--- mgmtsystem_nonconformity_repair/models/repair_order.py
+++ mgmtsystem_nonconformity_repair/models/repair_order.py
@@ -5,22 +5,17 @@ from odoo import api, fields, models
 
 
 class RepairOrder(models.Model):
-
-    _inherit = 'repair.order'
+    _inherit = "repair.order"
 
     mgmtsystem_nonconformity_ids = fields.One2many(
-        'mgmtsystem.nonconformity',
-        'repair_order_id',
-        string="Non-Conformities"
+        "mgmtsystem.nonconformity", "repair_order_id", string="Non-Conformities"
     )
 
     mgmtsystem_nonconformity_count = fields.Integer(
-        compute='_compute_mgmtsystem_nonconformity_count',
-        string='# Non-Conformities'
+        compute="_compute_mgmtsystem_nonconformity_count", string="# Non-Conformities"
     )
 
-    @api.depends('mgmtsystem_nonconformity_ids')
+    @api.depends("mgmtsystem_nonconformity_ids")
     def _compute_mgmtsystem_nonconformity_count(self):
         for rec in self:
-            rec.mgmtsystem_nonconformity_count = len(
-                rec.mgmtsystem_nonconformity_ids)
+            rec.mgmtsystem_nonconformity_count = len(rec.mgmtsystem_nonconformity_ids)
