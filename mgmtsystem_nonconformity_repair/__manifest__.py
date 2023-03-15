ec0554a94b56b12c05b91111ebc2a7f8a8aba34c
Rodrigo Neves <rodrigonevest@gmail.com>
2023-03-15 20:03:40 -0300

    [MIG] mgmtsystem_nonconformity_repair: Migration to 15.0

--- mgmtsystem_nonconformity_repair/__manifest__.py
+++ mgmtsystem_nonconformity_repair/__manifest__.py
@@ -5,7 +5,7 @@
     "name": "Mgmtsystem Nonconformity Repair",
     "summary": """
         Bridge module between Repair and Non Conformities""",
-    "version": "14.0.1.0.0",
+    "version": "15.0.1.0.0",
     "license": "AGPL-3",
     "author": "Escodoo,Odoo Community Association (OCA)",
     "website": "https://github.com/OCA/management-system",
