8f585c85f9dd944deb7d90a19a1d3d3b0c87a87d
Enric Tobella <etobella@creublanca.es>
2023-09-19 10:19:57 +0200

    [ADD] mgmtsystem_evaluation_hr

---
+++ mgmtsystem_evaluation_hr/__manifest__.py
@@ -0,0 +1,17 @@
+# Copyright 2023 Dixmit
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
+
+{
+    "name": "Mgmtsystem Evaluation Hr",
+    "summary": """
+        Allow to use evaluations on Employees""",
+    "version": "14.0.1.0.0",
+    "license": "AGPL-3",
+    "author": "Dixmit,Odoo Community Association (OCA)",
+    "website": "https://github.com/OCA/management-system",
+    "depends": ["mgmtsystem_evaluation", "hr"],
+    "data": [
+        "views/hr_employee.xml",
+    ],
+    "demo": [],
+}
