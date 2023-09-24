ceed8cf828c5e6c8dc2ee6623f6d84369e6b2af3
yassine-rhimi <81622165+yassine-rhimi@users.noreply.github.com>
2023-09-24 16:39:29 +0100

    Update mgmtsystem_audit.py

--- mgmtsystem_audit/__manifest__.py
+++ mgmtsystem_audit/__manifest__.py
@@ -2,7 +2,7 @@
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 {
     "name": "Management System - Audit",
-    "version": "16.0.1.0.0",
+    "version": "16.0.1.0.1",
     "author": "Savoir-faire Linux, Odoo Community Association (OCA)",
     "website": "https://github.com/OCA/management-system",
     "license": "AGPL-3",
