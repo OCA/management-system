bd64c7057178b93942f56ecedf9f716e0f26f7fa
Kaynnan Lemes <kaynnanx1@gmail.com>
2024-03-25 10:08:05 -0300

    [ADD] mgmtsystem_hazard_maintenance_equipment: add new module

---
+++ mgmtsystem_hazard_maintenance_equipment/__manifest__.py
@@ -0,0 +1,18 @@
+# Copyright 2024 - TODAY, Escodoo
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
+
+{
+    "name": "Management System - Maintenance Equipment",
+    "version": "16.0.1.0.0",
+    "author": "Escodoo, Odoo Community Association (OCA)",
+    "website": "https://github.com/OCA/management-system",
+    "license": "AGPL-3",
+    "category": "Management System",
+    "depends": [
+        "maintenance",
+        "mgmtsystem_hazard",
+    ],
+    "data": [
+        "views/mgmtsystem_hazard.xml",
+    ],
+}
---
+++ mgmtsystem_hazard_maintenance_equipment/readme/USAGE.rst
@@ -0,0 +1,5 @@
+To use this module, you need to:
+
+* Go to Management System → Documentation → Hazards
+* Create new Hazards
+* Select a Equipment
