074fe740ade83c6719cfe689f445729795651d49
Daniel Reis <dreis@opensourceintegrators.com>
2025-02-26 18:44:21 +0000

    [IMP] mgmtsystem_hazard_maintenance_equipment: pre-commit execution

--- mgmtsystem_hazard_maintenance_equipment/models/mgmtsystem_hazard.py
+++ mgmtsystem_hazard_maintenance_equipment/models/mgmtsystem_hazard.py
@@ -5,7 +5,6 @@ from odoo import fields, models
 
 
 class MgmtsystemHazard(models.Model):
-
     _inherit = "mgmtsystem.hazard"
 
     equipment_ids = fields.Many2many("maintenance.equipment", string="Equipment")
--- mgmtsystem_hazard_maintenance_equipment/readme/USAGE.rst
+++
@@ -1,5 +0,0 @@
-To use this module, you need to:
-
-* Go to Management System → Documentation → Hazards
-* Create new Hazards
-* Select a Equipment
