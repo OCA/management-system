1abc95866b60d32556eb932c9f2ef66b94fcdfad
Daniel Reis <dreis@opensourceintegrators.com>
2025-02-26 18:44:22 +0000

    [MIG] mgmtsystem_hazard_maintenance_equipment: Migration to 18.0

--- mgmtsystem_hazard_maintenance_equipment/__manifest__.py
+++ mgmtsystem_hazard_maintenance_equipment/__manifest__.py
@@ -3,7 +3,7 @@
 
 {
     "name": "Management System - Maintenance Equipment",
-    "version": "16.0.1.0.0",
+    "version": "18.0.1.0.0",
     "author": "Escodoo, Odoo Community Association (OCA)",
     "website": "https://github.com/OCA/management-system",
     "license": "AGPL-3",
