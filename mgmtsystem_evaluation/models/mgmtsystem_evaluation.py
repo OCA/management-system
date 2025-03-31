ace325672a914d93ef0ea4f3171d9388dea587b1
Enric Tobella <enric.tobella@dixmit.com>
2025-03-31 08:30:30 +0200

    [FIX] mgmtsystem_evaluation: Model must be searched with sudo

--- mgmtsystem_evaluation/models/mgmtsystem_evaluation.py
+++ mgmtsystem_evaluation/models/mgmtsystem_evaluation.py
@@ -104,7 +104,7 @@ class MgmtsystemEvaluation(models.Model):
 
     @api.model
     def _get_ref_selection(self):
-        models = self.env["ir.model"].search([])
+        models = self.env["ir.model"].sudo().search([])
         return [(model.model, model.name) for model in models]
 
     @api.depends("template_id", "res_id", "model")
