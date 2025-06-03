9e798cae4b519c2ff08b6892b6652a0c93550f42
Enric Tobella <enric.tobella@dixmit.com>
2025-06-03 18:10:14 +0200

    [FIX] mgmtsystem_nonconformity: Fix JS

--- mgmtsystem_nonconformity/static/src/components/chatter_topbar/chatter_topbar.esm.js
+++ mgmtsystem_nonconformity/static/src/components/chatter_topbar/chatter_topbar.esm.js
@@ -1,26 +1,22 @@
-/** @odoo-module **/
+import {Chatter} from "@mail/chatter/web_portal/chatter";
+import {patch} from "@web/core/utils/patch";
 
-import {registerPatch} from "@mail/model/model_core";
-
-registerPatch({
-    name: "Chatter",
-    recordMethods: {
-        async onClickShowNonConformities() {
-            if (this.isTemporary) {
-                const saved = await this.doSaveRecord();
-                if (!saved) {
-                    return;
-                }
+patch(Chatter.prototype, {
+    async onClickShowNonConformities() {
+        if (this.isTemporary) {
+            const saved = await this.doSaveRecord();
+            if (!saved) {
+                return;
             }
-            this.env.services.action.doAction(
-                "mgmtsystem_nonconformity.open_mgmtsystem_nonconformity_thread_list",
-                {
-                    additionalContext: {
-                        id: this.thread.id,
-                        mgmtsystem_nonconformity: this.thread.model,
-                    },
-                }
-            );
-        },
+        }
+        this.env.services.action.doAction(
+            "mgmtsystem_nonconformity.open_mgmtsystem_nonconformity_thread_list",
+            {
+                additionalContext: {
+                    id: this.props.threadId,
+                    mgmtsystem_nonconformity: this.props.threadModel,
+                },
+            }
+        );
     },
 });
