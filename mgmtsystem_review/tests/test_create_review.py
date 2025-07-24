b6740c4a4c8132b9b44bcf9cab5dd8976fb4f472
Víctor Martínez <victor.martinez@tecnativa.com>
2025-07-24 09:35:32 +0200

    [IMP] mgmtsystem_review: Improve tests (report)

--- mgmtsystem_review/tests/test_create_review.py
+++ mgmtsystem_review/tests/test_create_review.py
@@ -1,16 +1,25 @@
 from odoo import fields
-from odoo.tests import common
+
+from odoo.addons.base.tests.common import BaseCommon
 
 
-class TestModelReview(common.TransactionCase):
-    """Test class for mgmtsystem_review."""
-
-    def test_create_review(self):
-        """Test object creation."""
-        record = self.env["mgmtsystem.review"].create(
+class TestModelReviewBase(BaseCommon):
+    @classmethod
+    def setUpClass(cls):
+        super().setUpClass()
+        cls.review = cls.env["mgmtsystem.review"].create(
             {"name": "SampleReview", "date": fields.Datetime.now()}
         )
 
-        self.assertEqual(record.name, "SampleReview")
-        record.button_close()
-        self.assertEqual(record.state, "done")
+
+class TestModelReview(TestModelReviewBase):
+    """Test class for mgmtsystem_review."""
+
+    def test_review_misc(self):
+        self.assertNotEqual(self.review.reference, "NEW")
+        self.review.button_close()
+        self.assertEqual(self.review.state, "done")
+        res = self.env["ir.actions.report"]._render(
+            "mgmtsystem_review.review_report_template", self.review.ids
+        )
+        self.assertRegex(str(res[0]), "SampleReview")
