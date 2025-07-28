296b6088e35f7e1423d6960573f16709840d1dac
Víctor Martínez <victor.martinez@tecnativa.com>
2025-07-28 16:28:09 +0200

    [IMP] mgmtsystem_audit: Misc changes
    - Define the sequence field with the appropriate widget and in the first column to avoid confusion.
    - Add the possibility to print reports
    - Improve tests

--- mgmtsystem_audit/tests/test_audit.py
+++ mgmtsystem_audit/tests/test_audit.py
@@ -5,34 +5,73 @@ from odoo.tests import common
 
 
 class TestModelAudit(common.TransactionCase):
+    @classmethod
+    def setUpClass(cls):
+        super().setUpClass()
+        cls.env = cls.env(
+            context=dict(
+                cls.env.context,
+                mail_create_nolog=True,
+                mail_create_nosubscribe=True,
+                mail_notrack=True,
+                no_reset_password=True,
+                tracking_disable=True,
+            )
+        )
+        cls.page = cls.env.ref("document_page.demo_page1")
+        cls.audit = cls.env["mgmtsystem.audit"].create(
+            {
+                "name": "Test audit",
+                "date": "2025-01-01",
+                "line_ids": [
+                    (0, 0, {"name": "test", "procedure_id": cls.page.id}),
+                    (0, 0, {"name": "test2"}),
+                ],
+            }
+        )
+
     def test_get_action_url(self):
         """Test if action url start with http."""
-
-        record = self.env.ref("mgmtsystem_audit.mgmtsystem_audit_demo")
-
-        ret = record.get_action_url()
-
+        ret = self.audit.get_action_url()
         self.assertEqual(isinstance(ret, str), True)
         self.assertEqual(ret.startswith("http"), True)
 
     def test_button_close(self):
         """Test if button close change audit state to close."""
-        record = self.env.ref("mgmtsystem_audit.mgmtsystem_audit_demo")
-        record.state = "open"
-        self.assertEqual(record.state, "open")
-        record.button_close()
-        self.assertEqual(record.state, "done")
+        self.audit.state = "open"
+        self.assertEqual(self.audit.state, "open")
+        self.audit.button_close()
+        self.assertEqual(self.audit.state, "done")
 
     def test_get_lines_by_procedure(self):
-        line_id = self.env["mgmtsystem.verification.line"].create(
+        self.assertTrue(self.audit.get_lines_by_procedure())
+
+    def test_copyVerificationLines(self):
+        dest_record = self.env["mgmtsystem.audit"].create(
             {
-                "name": "test",
-                "procedure_id": self.env.ref("document_page.demo_page1").id,
+                "name": "Test audit2",
+                "date": "2025-01-01",
             }
         )
-        line_id2 = self.env["mgmtsystem.verification.line"].create({"name": "test2"})
+        copy_record = self.env["copy.verification.lines"].create(
+            {"audit_src": self.audit.id}
+        )
+        copy_record = copy_record.with_context(
+            active_id=dest_record.id, active_model=self.audit._name
+        )
+        copy_record.copyVerificationLines()
+        self.assertGreater(len(dest_record.line_ids), 0)
 
-        record = self.env.ref("mgmtsystem_audit.mgmtsystem_audit_demo")
-        record.line_ids = [line_id.id, line_id2.id]
-        q = record.get_lines_by_procedure()
-        self.assertTrue(q)
+    def test_audit_reports(self):
+        res = (
+            self.env["ir.actions.report"]
+            ._get_report_from_name("mgmtsystem_audit.audit_report_template")
+            ._render_qweb_text(self.audit.ids, False)
+        )
+        self.assertRegex(str(res[0]), "Test audit")
+        res = (
+            self.env["ir.actions.report"]
+            ._get_report_from_name("mgmtsystem_audit.verification_report_template")
+            ._render_qweb_text(self.audit.ids, False)
+        )
+        self.assertRegex(str(res[0]), "Test audit")
