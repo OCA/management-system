4cc9f197ca0293667b93afcf5c8282c2294662d5
Víctor Martínez <victor.martinez@tecnativa.com>
2025-07-29 09:06:01 +0200

    [IMP] document_page_procedure: Define data without company
    TT57340

--- document_page_procedure/__manifest__.py
+++ document_page_procedure/__manifest__.py
@@ -3,7 +3,7 @@
 
 {
     "name": "Document Management - Wiki - Procedures",
-    "version": "18.0.1.0.0",
+    "version": "18.0.1.0.1",
     "author": "Savoir-faire Linux, Odoo Community Association (OCA)",
     "website": "https://github.com/OCA/management-system",
     "license": "AGPL-3",
