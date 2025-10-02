45b337e0e64c3306db61adfd3c80c208d67aff08
Geyson Gualdron <geyson@trey.es>
2025-10-02 11:01:23 +0200

    [MIG] mgmtsystem_nonconformity_project: Migration to 16.0

---
+++ mgmtsystem_nonconformity_project/tests/test_mgmtsystem_nonconformity_project.py
@@ -0,0 +1,35 @@
+from odoo.tests import common
+
+
+class TestMgmtsystemNonconformityProject(common.TransactionCase):
+    @classmethod
+    def setUpClass(cls):
+        super().setUpClass()
+        # Create a project
+        cls.project = cls.env["project.project"].create(
+            {
+                "name": "Test Project",
+            }
+        )
+        # Create a nonconformity action
+        cls.action = cls.env["mgmtsystem.action"].create(
+            {
+                "name": "Test Action",
+                "type_action": "immediate",
+                "action_type": "action",
+            }
+        )
+
+    def test_action_type_selection(self):
+        """Test that action_type field works correctly."""
+        self.assertEqual(self.action.action_type, "action")
+
+    def test_complete_name_action_type(self):
+        """Test complete_name computation for action type."""
+        self.assertEqual(self.action.complete_name, "Test Action")
+
+    def test_complete_name_project_type(self):
+        """Test complete_name computation for project type."""
+        self.action.action_type = "project"
+        self.action.project_id = self.project
+        self.assertEqual(self.action.complete_name, "Test Project")
