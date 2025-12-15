9be49660e3750625bbc80d712329064e6617f233
Enric Tobella <enric.tobella@dixmit.com>
2025-12-15 15:43:56 +0100

    [IMP] mgmtsystem_manual: use type

--- mgmtsystem_manual/__manifest__.py
+++ mgmtsystem_manual/__manifest__.py
@@ -7,8 +7,8 @@
     "author": "Savoir-faire Linux,Gray Matter Logic, Odoo Community Association (OCA)",
     "website": "https://github.com/OCA/management-system",
     "license": "AGPL-3",
-    "category": "Management Systems",
-    "depends": ["document_page", "mgmtsystem"],
+    "category": "Management System",
+    "depends": ["document_page_mgmtsystem", "mgmtsystem"],
     "data": [
         "data/mgmtsystem_manual.xml",
         "views/mgmtsystem_manual.xml",
