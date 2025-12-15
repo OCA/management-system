c441a01c5261e57944d8bf4b81932e37091b6407
Enric Tobella <enric.tobella@dixmit.com>
2025-12-15 15:41:23 +0100

    [ADD] document_page_mgmtsystem

---
+++ document_page_mgmtsystem/__manifest__.py
@@ -0,0 +1,18 @@
+# Copyright 2025 Dixmit
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
+
+{
+    "name": "Document Page Mgmtsystem",
+    "summary": """Add a new mgmtsystem type field for classification""",
+    "version": "18.0.1.0.0",
+    "license": "AGPL-3",
+    "author": "Dixmit,Odoo Community Association (OCA)",
+    "website": "https://github.com/OCA/management-system",
+    "depends": [
+        "document_page",
+    ],
+    "data": [
+        "views/document_page.xml",
+    ],
+    "demo": [],
+}
