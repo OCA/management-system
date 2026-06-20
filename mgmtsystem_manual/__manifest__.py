ab62727b6efb4c4f80e7af84befb3805ad157f16
Maxime Chambreuil <mchambreuil@opensourceintegrators.com>
2026-06-19 22:46:38 -0600

    [MIG] mgmtsystem_manual: Migrate to 19.0

--- mgmtsystem_manual/__manifest__.py
+++ mgmtsystem_manual/__manifest__.py
@@ -4,10 +4,10 @@
 {
     "name": "Management System - Manual",
     "version": "19.0.1.0.0",
-    "author": "Savoir-faire Linux,Odoo Community Association (OCA)",
+    "author": "Savoir-faire Linux,Gray Matter Logic, Odoo Community Association (OCA)",
     "website": "https://github.com/OCA/management-system",
     "license": "AGPL-3",
-    "category": "Management System",
+    "category": "Management Systems",
     "depends": ["document_page", "mgmtsystem"],
     "data": [
         "data/mgmtsystem_manual.xml",
