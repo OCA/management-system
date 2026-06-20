c1731679af8e55d0b0007beb2a2b0be210d11241
Maxime Chambreuil <mchambreuil@opensourceintegrators.com>
2026-06-19 22:45:10 -0600

    [IMP] mgmtsystem_information_security: Fix author, category and update icon.png

--- mgmtsystem_information_security/__manifest__.py
+++ mgmtsystem_information_security/__manifest__.py
@@ -5,10 +5,10 @@
     "name": "Information Security Management System",
     "summary": "Manage your ISMS",
     "version": "19.0.1.0.0",
-    "author": "Savoir-faire Linux, Odoo Community Association (OCA)",
+    "author": "Savoir-faire Linux, Gray Matter Logic, Odoo Community Association (OCA)",
     "website": "https://github.com/OCA/management-system",
     "license": "AGPL-3",
-    "category": "Management System",
+    "category": "Management Systems",
     "depends": [
         "mgmtsystem_manual",
         "mgmtsystem_info_security_manual",
