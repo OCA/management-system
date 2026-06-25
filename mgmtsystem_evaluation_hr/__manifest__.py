2c093a2076542d4881468997a4e9f7fa1b6812e8
gzoccarato <zoccarato.gianluca@gmail.com>
2026-06-25 11:59:13 +0200

    Migration to version 18.0

--- mgmtsystem_evaluation_hr/__manifest__.py
+++ mgmtsystem_evaluation_hr/__manifest__.py
@@ -5,7 +5,7 @@
     "name": "Mgmtsystem Evaluation Hr",
     "summary": """
         Allow to use evaluations on Employees""",
-    "version": "16.0.1.0.0",
+    "version": "18.0.1.0.0",
     "license": "AGPL-3",
     "author": "Dixmit,Odoo Community Association (OCA)",
     "website": "https://github.com/OCA/management-system",
